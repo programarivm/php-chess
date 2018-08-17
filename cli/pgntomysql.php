74e82b6d88b8353b641514dbfebeecd8543d51ae
programarivm <info@programarivm.com>
2018-08-17 21:10:54 +0100

    cli/pgntomysql.php loads env variables

--- cli/pgntomysql.php
+++ cli/pgntomysql.php
@@ -2,9 +2,13 @@
 
 namespace PGNChess\Cli;
 
+use Dotenv\Dotenv;
 use PGNChess\PGN\File\ToMySql as PgnFileToMySql;
 
 require_once __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = new Dotenv(__DIR__.'/../');
+$dotenv->load();
 
 $sql = (new PgnFileToMySql($argv[1]))->convert();
 
