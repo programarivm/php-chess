9e08601317d777ee15ff1b7a6547387703c69223
programarivm <info@programarivm.com>
2018-08-18 22:46:06 +0100

    Validate::movetext() passes the tests

--- src/PGN/Validate.php
+++ src/PGN/Validate.php
@@ -6,6 +6,8 @@ use PGNChess\Exception\UnknownNotationException;
 use PGNChess\PGN\Symbol;
 use PGNChess\PGN\Tag;
 
+use PGNChess\PGN\Convert;
+
 /**
  * Validates PGN symbols.
  *
@@ -154,12 +156,48 @@ class Validate
     /**
      * Validates a PGN movetext.
      *
-     * @param \PGNChess\Game $game
+     * @param string $movetext
      * @return bool true if the movetext is valid; otherwise false
+     * @throws \PGNChess\Exception\UnknownNotationException
      */
-    public static function movetext($game)
+    public static function movetext($movetext)
     {
-        // TODO ...
+        $numbers = [];
+        $notations = [];
+        $moves = array_filter(explode(' ', $movetext));
+
+        foreach ($moves as $move) {
+            if (preg_match('/^[1-9][0-9]*\.(.*)$/', $move)) {
+                $moveExploded = explode('.', $move);
+                $numbers[] = $moveExploded[0];
+                $notations[] = $moveExploded[1];
+            } else {
+                $notations[] = $move;
+            }
+        }
+
+        $areConsecutiveNumbers = 1;
+        for ($i = 0; $i < count($numbers); $i++) {
+            $areConsecutiveNumbers *= (int) $numbers[$i] == $i + 1;
+        }
+        
+        if (!$areConsecutiveNumbers) {
+            return false;
+        }
+
+        $notations = array_filter($notations);
+        foreach ($notations as $move) {
+            if ($move !== Symbol::RESULT_WHITE_WINS &&
+                $move !== Symbol::RESULT_BLACK_WINS &&
+                $move !== Symbol::RESULT_DRAW
+            ) {
+                try {
+                    Convert::toObject(Symbol::WHITE, $move);
+                } catch (UnknownNotationException $e) {
+                    return false;
+                }
+            }
+        }
 
         return true;
     }
