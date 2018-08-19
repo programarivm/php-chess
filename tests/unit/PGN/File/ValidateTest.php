534e25e2282b90eb33195fc07c95941036427ea1
programarivm <info@programarivm.com>
2018-08-19 21:44:11 +0100

    testing text files with non str games

--- tests/unit/PGN/File/ValidateTest.php
+++ tests/unit/PGN/File/ValidateTest.php
@@ -89,4 +89,25 @@ class ValidateTest extends TestCase
             ['text-03.pgn'],
         ];
     }
+
+    /**
+     * @dataProvider textWithNonStrGamesData
+     * @test
+     */
+    public function syntax_text_with_non_str_games($filename, $nErrors)
+    {
+        $result = (new PgnFileValidate(self::DATA_FOLDER."/$filename"))->syntax();
+
+        $this->assertEquals(0, $result->valid);
+        $this->assertEquals($nErrors, count($result->errors));
+    }
+
+    public function textWithNonStrGamesData()
+    {
+        return [
+            ['text-with-non-str-games-01.pgn', 1],
+            ['text-with-non-str-games-02.pgn', 4],
+            ['text-with-non-str-games-03.pgn', 2],
+        ];
+    }
 }
