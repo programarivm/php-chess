f24903e57ccdf41d3b9054d473c37a3f2b6d00cf
programarivm <info@programarivm.com>
2018-08-23 22:28:10 +0100

    using type hinting and return type declarations in PGNChess\PGN

--- src/PGN/Convert.php
+++ src/PGN/Convert.php
@@ -29,7 +29,7 @@ class Convert
      * @return \stdClass
      * @throws \PGNChess\Exception\UnknownNotationException
      */
-    public static function toObject($color, $pgn)
+    public static function toObject(string $color, string $pgn): \stdClass
     {
         $isCheck = substr($pgn, -1) === '+' || substr($pgn, -1) === '#';
 
@@ -226,7 +226,7 @@ class Convert
      * @param string $identity
      * @return string
      */
-    public static function toClassName($identity)
+    public static function toClassName(string $identity): string
     {
         switch($identity) {
             case Symbol::BISHOP:
