3b6e963348b78cf612ac5b0d5ef99bd2f822fb3a
programarivm <info@programarivm.com>
2018-08-24 16:10:01 +0100

    tests/integration/PGN/File/ConvertTest.php fix

--- tests/integration/PGN/File/ConvertTest.php
+++ tests/integration/PGN/File/ConvertTest.php
@@ -26,6 +26,8 @@ class ConvertTest extends TestCase
     public function to_mysql_games($filename)
     {
         $sql = (new PgnFileConvert(self::DATA_FOLDER."/$filename"))->toMySqlScript();
+
+        $this->assertTrue(strpos($sql, 'INSERT INTO games') === 0);
     }
 
     public function pgnData()
