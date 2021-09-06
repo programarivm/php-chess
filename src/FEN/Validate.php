855956c769ab7e26f44f719e255fed1a986cc7ca
programarivm <info@programarivm.com>
2021-09-06 12:41:02 +0200

    Passed castling tests

--- src/FEN/Validate.php
+++ src/FEN/Validate.php
@@ -14,24 +14,27 @@ class Validate
 {
     public static function length(string $fen): bool
     {
-        $exploded = explode(' ', $fen);
-        if (count($exploded) !== 6) {
-            throw new UnknownNotationException(
-                "This FEN string does not consist of six fields separated by a space char."
-            );
+        if (count(explode(' ', $fen)) === 6) {
+            return true;
         }
 
-        return true;
+        throw new UnknownNotationException(
+            "The FEN string should consist of six fields separated by a space char."
+        );
     }
 
     public static function castling(string $ability): bool
     {
-        if (!preg_match('/^K?Q?k?q?$/', $ability)) {
-            throw new UnknownNotationException(
-                "This FEN string does not contain a valid castling ability."
-            );
+        if ($ability) {
+            if ('-' === $ability) {
+                return true;
+            } elseif (preg_match('/^K?Q?k?q?$/', $ability)) {
+                return true;
+            }
         }
 
-        return true;
+        throw new UnknownNotationException(
+            "This FEN string does not contain a valid castling ability."
+        );
     }
 }
--- tests/unit/FEN/Validate/CastlingAbilityTest.php
+++ tests/unit/FEN/Validate/CastlingAbilityTest.php
@@ -11,8 +11,124 @@ class CastlingAbilityTest extends AbstractUnitTestCase
     /**
      * @test
      */
-    public function K()
+    public function foobar()
+    {
+        $this->expectException(UnknownNotationException::class);
+
+        $this->assertTrue(Validate::castling('foobar'));
+    }
+
+    /**
+     * @test
+     */
+    public function start_b_kqKQ()
+    {
+        $this->expectException(UnknownNotationException::class);
+
+        $this->assertTrue(Validate::castling('kqKQ'));
+    }
+
+    /**
+     * @test
+     */
+    public function start_rearrange_KkQq()
+    {
+        $this->expectException(UnknownNotationException::class);
+
+        $this->assertTrue(Validate::castling('KkQq'));
+    }
+
+    /**
+     * @test
+     */
+    public function start_w_KQkq()
+    {
+        $this->assertTrue(Validate::castling('KQkq'));
+    }
+
+    /**
+     * @test
+     */
+    public function w_k()
     {
         $this->assertTrue(Validate::castling('K'));
+    }
+
+    /**
+     * @test
+     */
+    public function w_q()
+    {
+        $this->assertTrue(Validate::castling('Q'));
+    }
+
+    /**
+     * @test
+     */
+    public function b_k()
+    {
+        $this->assertTrue(Validate::castling('k'));
+    }
+
+    /**
+     * @test
+     */
+    public function b_q()
+    {
+        $this->assertTrue(Validate::castling('q'));
+    }
+
+    /**
+     * @test
+     */
+    public function w_kq()
+    {
+        $this->assertTrue(Validate::castling('KQ'));
+    }
+
+    /**
+     * @test
+     */
+    public function b_kq()
+    {
+        $this->assertTrue(Validate::castling('kq'));
+    }
+
+    /**
+     * @test
+     */
+    public function hyphen()
+    {
+        $this->assertTrue(Validate::castling('-'));
+    }
+
+    /**
+     * @test
+     */
+    public function double_hyphen()
+    {
+        $this->expectException(UnknownNotationException::class);
+
+        $this->assertTrue(Validate::castling('--'));
+    }
+
+    /**
+     * @test
+     */
+    public function b_k_hyphen()
+    {
+        $this->expectException(UnknownNotationException::class);
+
+        $this->assertTrue(Validate::castling('k-'));
+    }
+
+    /**
+     * @test
+     */
+    public function empty_string()
+    {
+        $this->expectException(UnknownNotationException::class);
+
+        $this->assertTrue(Validate::castling(''));
     }
 }
