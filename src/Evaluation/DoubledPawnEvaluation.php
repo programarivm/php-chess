410c5ca2f011a15b00ef3bdc44343ee83beacb95
programarivm <info@programarivm.com>
2021-09-23 18:55:29 +0200

    Added file DoubledPawnEvaluation.php

---
+++ src/Evaluation/DoubledPawnEvaluation.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Chess\Evaluation;
+
+use Chess\Board;
+use Chess\PGN\Symbol;
+
+class DoubledPawnEvaluation extends AbstractEvaluation
+{
+    public function __construct(Board $board)
+    {
+        parent::__construct($board);
+
+        $this->result = [
+            Symbol::WHITE => 0,
+            Symbol::BLACK => 0,
+        ];
+    }
+
+    public function evaluate($feature = null): array
+    {
+        // TODO
+
+        return $this->result;
+    }
+}
