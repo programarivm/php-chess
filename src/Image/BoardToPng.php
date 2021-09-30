2fe47988f635e847c9600220b0acd0b9c6c15134
programarivm <info@programarivm.com>
2021-09-30 18:29:29 +0200

    Passed test output_01_starting()

--- src/Image/BoardToPng.php
+++ src/Image/BoardToPng.php
@@ -5,7 +5,6 @@ namespace Chess\Image;
 use Chess\Ascii;
 use Chess\Board;
 use Imagine\Gd\Imagine;
-use Imagine\Image\Box;
 use Imagine\Image\Point;
 
 class BoardToPng
@@ -25,17 +24,21 @@ class BoardToPng
 
     public function output(string $filepath)
     {
-        $array = (new Ascii())->toArray($this->board);
-
-        $wPawn = $this->imagine->open(self::FILEPATH . '/pieces/wPawn.png');
         $chessboard = $this->imagine->open(self::FILEPATH . '/chessboard.png');
-
-        $wPawnSize = $wPawn->getSize();
-        $chessboardSize = $chessboard->getSize();
-
-        $bottomRight = new Point($chessboardSize->getWidth() - $wPawnSize->getWidth(), $chessboardSize->getHeight() - $wPawnSize->getHeight());
-
-        $chessboard->paste($wPawn, $bottomRight);
+        $array = (new Ascii())->toArray($this->board);
+        $x = $y = 0;
+        foreach ($array as $i => $rank) {
+            foreach ($rank as $j => $piece) {
+                if ($piece !== ' . ') {
+                    $filename = trim($piece);
+                    $image = $this->imagine->open(self::FILEPATH . "/pieces/$filename.png");
+                    $chessboard->paste($image, new Point($x, $y));
+                }
+                $x += 90;
+            }
+            $x = 0;
+            $y += 90;
+        }
 
         $chessboard->save($filepath);
     }
--- tests/unit/Image/BoardToPngTest.php
+++ tests/unit/Image/BoardToPngTest.php
@@ -8,19 +8,20 @@ use Chess\Tests\AbstractUnitTestCase;
 
 class BoardToPngTest extends AbstractUnitTestCase
 {
-    const FILEPATH = __DIR__.'/../../data';
+    const OUTPUT_FOLDER = __DIR__.'/../../output';
 
     /**
      * @test
      */
-    public function output_foo_png()
+    public function output_01_starting()
     {
         $board = new Board();
 
-        (new BoardToPng($board))->output(self::FILEPATH . '/foo.png');
+        (new BoardToPng($board))->output(self::OUTPUT_FOLDER . '/tmp.png');
 
-        // TODO
-
-        $this->assertTrue(false);
+        $this->assertEquals(
+            md5(file_get_contents(self::OUTPUT_FOLDER . '/tmp.png')),
+            md5(file_get_contents(self::DATA_FOLDER . '/img/01_starting.png'))
+        );
     }
 }
