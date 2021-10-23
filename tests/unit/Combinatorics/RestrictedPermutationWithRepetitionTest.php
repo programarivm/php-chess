aa76dcf8b31defed14283a7ea3e1026ada91d374
programarivm <info@programarivm.com>
2021-10-23 11:29:32 +0200

    Added setUpBeforeClass() method

--- tests/unit/Combinatorics/RestrictedPermutationWithRepetitionTest.php
+++ tests/unit/Combinatorics/RestrictedPermutationWithRepetitionTest.php
@@ -7,13 +7,21 @@ use Chess\Tests\AbstractUnitTestCase;
 
 class RestrictedPermutationWithRepetitionTest extends AbstractUnitTestCase
 {
+    public static $permutation;
+
+    public static function setUpBeforeClass(): void
+    {
+        self::$permutation = new RestrictedPermutationWithRepetition();
+    }
+
     /**
      * @test
      */
     public function permutation_8_13_21_34_count()
     {
-        $set = (new RestrictedPermutationWithRepetition())->get([8, 13, 21, 34], 8, 100);
+        $set = self::$permutation->get([8, 13, 21, 34], 8, 100);
         $count = count($set);
+
         $expected = 588;
 
         $this->assertEquals($expected, $count);
@@ -24,8 +32,9 @@ class RestrictedPermutationWithRepetitionTest extends AbstractUnitTestCase
      */
     public function permutation_8_13_21_34_first()
     {
-        $set = (new RestrictedPermutationWithRepetition())->get([8, 13, 21, 34], 8, 100);
+        $set = self::$permutation->get([8, 13, 21, 34], 8, 100);
         $first = $set[0];
+
         $expected = [ 34, 13, 13, 8, 8, 8, 8, 8 ];
 
         $this->assertEquals($expected, $first);
@@ -36,8 +45,9 @@ class RestrictedPermutationWithRepetitionTest extends AbstractUnitTestCase
      */
     public function permutation_8_13_21_34_last()
     {
-        $set = (new RestrictedPermutationWithRepetition())->get([8, 13, 21, 34], 8, 100);
+        $set = self::$permutation->get([8, 13, 21, 34], 8, 100);
         $last = end($set);
+
         $expected = [ 8, 8, 8, 8, 8, 13, 13, 34 ];
 
         $this->assertEquals($expected, $last);
@@ -48,8 +58,9 @@ class RestrictedPermutationWithRepetitionTest extends AbstractUnitTestCase
      */
     public function permutation_8_13_21_34_start()
     {
-        $set = (new RestrictedPermutationWithRepetition())->get([8, 13, 21, 34], 8, 100);
+        $set = self::$permutation->get([8, 13, 21, 34], 8, 100);
         $start = array_slice($set, 0, 5);
+
         $expected = [
             [ 34, 13, 13, 8, 8, 8, 8, 8 ],
             [ 13, 34, 13, 8, 8, 8, 8, 8 ],
@@ -66,8 +77,9 @@ class RestrictedPermutationWithRepetitionTest extends AbstractUnitTestCase
      */
     public function permutation_8_13_21_34_end()
     {
-        $set = (new RestrictedPermutationWithRepetition())->get([8, 13, 21, 34], 8, 100);
+        $set = self::$permutation->get([8, 13, 21, 34], 8, 100);
         $end = array_slice($set, -6);
+
         $expected = [
             [ 13, 8, 8, 8, 8, 8, 13, 34 ],
             [ 8, 13, 8, 8, 8, 8, 13, 34 ],
@@ -85,8 +97,9 @@ class RestrictedPermutationWithRepetitionTest extends AbstractUnitTestCase
      */
     public function permutation_3_5_8_13_21_count()
     {
-        $set = (new RestrictedPermutationWithRepetition())->get([3, 5, 8, 13, 21], 9, 100);
+        $set = self::$permutation->get([3, 5, 8, 13, 21], 9, 100);
         $count = count($set);
+
         $expected = 56448;
 
         $this->assertEquals($expected, $count);
@@ -97,8 +110,9 @@ class RestrictedPermutationWithRepetitionTest extends AbstractUnitTestCase
      */
     public function permutation_3_5_8_13_21_first()
     {
-        $set = (new RestrictedPermutationWithRepetition())->get([3, 5, 8, 13, 21], 9, 100);
+        $set = self::$permutation->get([3, 5, 8, 13, 21], 9, 100);
         $first = $set[0];
+
         $expected = [ 21, 21, 21, 13, 8, 5, 5, 3, 3 ];
 
         $this->assertEquals($expected, $first);
@@ -109,8 +123,9 @@ class RestrictedPermutationWithRepetitionTest extends AbstractUnitTestCase
      */
     public function permutation_3_5_8_13_21_last()
     {
-        $set = (new RestrictedPermutationWithRepetition())->get([3, 5, 8, 13, 21], 9, 100);
+        $set = self::$permutation->get([3, 5, 8, 13, 21], 9, 100);
         $last = end($set);
+
         $expected = [ 3, 3, 5, 5, 8, 13, 21, 21, 21 ];
 
         $this->assertEquals($expected, $last);
