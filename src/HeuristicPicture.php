eb668cb03be79190907b2416d25695b8712cbb45
vikeng <vikengui@gmail.com>
2021-10-24 21:12:54 +0800

    Add the doubled pawn evaluation to the chess heuristic picture #71

--- src/HeuristicPicture.php
+++ src/HeuristicPicture.php
@@ -14,11 +14,12 @@ use Chess\Evaluation\SquareEvaluation;
 use Chess\Evaluation\TacticsEvaluation;
 use Chess\PGN\Convert;
 use Chess\PGN\Symbol;
+use Chess\Evaluation\DoubledPawnEvaluation;
 
 class HeuristicPicture extends Player
 {
     protected $dimensions = [
-        MaterialEvaluation::class => 34,
+        MaterialEvaluation::class => 26,
         CenterEvaluation::class => 13,
         ConnectivityEvaluation::class => 13,
         SpaceEvaluation::class => 8,
@@ -26,6 +27,7 @@ class HeuristicPicture extends Player
         KingSafetyEvaluation::class => 8,
         TacticsEvaluation::class => 8,
         AttackEvaluation::class => 8,
+        DoubledPawnEvaluation::class => 8,
     ];
 
     protected $picture = [];
