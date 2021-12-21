ccff23d8c579f2d40c2008d0092223b647f67d91
= <nicola.fanelli16@gmail.com>
2021-12-21 09:37:16 +0100

    Test FIDE notation

--- tests/unit/PGN/Validate/MovetextTest.php
+++ tests/unit/PGN/Validate/MovetextTest.php
@@ -25,6 +25,15 @@ class MovetextTest extends AbstractUnitTestCase
     }
 
     /**
+     * @dataProvider validDataFIDE
+     * @test
+     */
+    public function validFIDE($movetext)
+    {
+        $this->assertSame($movetext, Validate::movetext($movetext));
+    }
+
+    /**
      * @dataProvider commentsRemovedData
      * @test
      */
@@ -115,6 +124,19 @@ class MovetextTest extends AbstractUnitTestCase
                 '1.e4 c5 2.Nf3 Nc6 3.FOO cxd4 4.Nxd4 Nf6 5.Nc3 e5 6.Ndb5 d6 7.Bg5 a6 8.Na3',
                 '1.d4 Nf6 2.c4 e6 3.Nc3 Bb4 4.e3 O-O 5.a3 Bxc3+ 6.bxc3 b6 7.Bd3 Bb7 8.f3 BAR',
                 '1.Nf3 Nf6 2.c4 c5 3.g3 BAR 4.Bg2 FOO 5.O-O e6 6.FOOBAR 7.d4 cxd4 8.Qxd4 d6',
+            ],
+        ];
+    }
+
+    public function validDataFIDE()
+    {
+        return [
+            [
+                '1.d4 Nf6 2.Nf3 e6 3.c4 Bb4+ 4.Nbd2 0-0 5.a3 Be7 6.e4 d6 7.Bd3 c5',
+                '1.e4 Nf6 2.e5 Nd5 3.d4 d6 4.Nf3 dxe5 5.Nxe5 c6 6.Be2 Bf5 7.c3 Nd7',
+                '1.e4 c5 2.Nf3 Nc6 3.d4 cxd4 4.Nxd4 Nf6 5.Nc3 e5 6.Ndb5 d6 7.Bg5 a6 8.Na3',
+                '1.d4 Nf6 2.c4 e6 3.Nc3 Bb4 4.e3 0-0 5.a3 Bxc3+ 6.bxc3 b6 7.Bd3 Bb7 8.f3 c5',
+                '1.Nf3 Nf6 2.c4 c5 3.g3 b6 4.Bg2 Bb7 5.0-0 e6 6.Nc3 a6 7.d4 cxd4 8.Qxd4 d6',
             ],
         ];
     }
