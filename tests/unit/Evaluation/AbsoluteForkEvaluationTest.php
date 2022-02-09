fe4d168f2f7ab83a563d224337588ae1bb2b7577
programarivm <info@programarivm.com>
2022-02-09 12:58:14 +0100

    Passed test pawn_attacks_king_and_knight()

--- tests/unit/Evaluation/AbsoluteForkEvaluationTest.php
+++ tests/unit/Evaluation/AbsoluteForkEvaluationTest.php
@@ -26,4 +26,40 @@ class AbsoluteForkEvaluationTest extends AbstractUnitTestCase
 
         $this->assertSame($expected, $absForkEvald);
     }
+
+    /**
+     * @test
+     */
+    public function pawn_attacks_bishop_and_knight()
+    {
+        $board = (new StringToBoard('8/1k6/5b1n/6P1/7K/8/8/8 w - -'))
+            ->create();
+
+        $expected = [
+            'w' => 0,
+            'b' => 0,
+        ];
+
+        $absForkEvald = (new AbsoluteForkEvaluation($board))->evaluate();
+
+        $this->assertSame($expected, $absForkEvald);
+    }
+
+    /**
+     * @test
+     */
+    public function pawn_attacks_king_and_knight()
+    {
+        $board = (new StringToBoard('8/8/5k1n/6P1/7K/8/8/8 w - -'))
+            ->create();
+
+        $expected = [
+            'w' => 3.2,
+            'b' => 0,
+        ];
+
+        $absForkEvald = (new AbsoluteForkEvaluation($board))->evaluate();
+
+        $this->assertSame($expected, $absForkEvald);
+    }
 }
