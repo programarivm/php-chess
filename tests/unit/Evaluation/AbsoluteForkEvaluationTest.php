45d45b49984001511f9f01ad385c473cfabeac94
programarivm <info@programarivm.com>
2022-02-09 13:11:03 +0100

    Added tests

--- tests/unit/Evaluation/AbsoluteForkEvaluationTest.php
+++ tests/unit/Evaluation/AbsoluteForkEvaluationTest.php
@@ -30,7 +30,7 @@ class AbsoluteForkEvaluationTest extends AbstractUnitTestCase
     /**
      * @test
      */
-    public function pawn_attacks_bishop_and_knight()
+    public function pawn_forks_bishop_and_knight()
     {
         $board = (new StringToBoard('8/1k6/5b1n/6P1/7K/8/8/8 w - -'))
             ->create();
@@ -48,13 +48,85 @@ class AbsoluteForkEvaluationTest extends AbstractUnitTestCase
     /**
      * @test
      */
-    public function pawn_attacks_king_and_knight()
+    public function pawn_forks_king_and_knight()
     {
         $board = (new StringToBoard('8/8/5k1n/6P1/7K/8/8/8 w - -'))
             ->create();
 
         $expected = [
             'w' => 3.2,
+            'b' => 0,
+        ];
+
+        $absForkEvald = (new AbsoluteForkEvaluation($board))->evaluate();
+
+        $this->assertSame($expected, $absForkEvald);
+    }
+
+    /**
+     * @test
+     */
+    public function pawn_forks_king_and_rook()
+    {
+        $board = (new StringToBoard('8/8/5k1r/6P1/7K/8/8/8 w - -'))
+            ->create();
+
+        $expected = [
+            'w' => 5.1,
+            'b' => 0,
+        ];
+
+        $absForkEvald = (new AbsoluteForkEvaluation($board))->evaluate();
+
+        $this->assertSame($expected, $absForkEvald);
+    }
+
+    /**
+     * @test
+     */
+    public function pawn_forks_king_and_queen()
+    {
+        $board = (new StringToBoard('8/8/5k1q/6P1/7K/8/8/8 w - -'))
+            ->create();
+
+        $expected = [
+            'w' => 8.8,
+            'b' => 0,
+        ];
+
+        $absForkEvald = (new AbsoluteForkEvaluation($board))->evaluate();
+
+        $this->assertSame($expected, $absForkEvald);
+    }
+
+    /**
+     * @test
+     */
+    public function knight_forks_king_and_bishop()
+    {
+        $board = (new StringToBoard('8/8/1k6/4b1P1/2N4K/8/8/8 w - -'))
+            ->create();
+
+        $expected = [
+            'w' => 3.33,
+            'b' => 0,
+        ];
+
+        $absForkEvald = (new AbsoluteForkEvaluation($board))->evaluate();
+
+        $this->assertSame($expected, $absForkEvald);
+    }
+
+    /**
+     * @test
+     */
+    public function knight_forks_king_and_knight()
+    {
+        $board = (new StringToBoard('8/8/1k6/4n1P1/2N4K/8/8/8 w - -'))
+            ->create();
+
+        $expected = [
+            'w' => 0,
             'b' => 0,
         ];
 
