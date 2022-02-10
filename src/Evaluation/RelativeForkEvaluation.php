9ceb4a65455a932221d4c20fb858587deadc1136
programarivm <info@programarivm.com>
2022-02-10 12:27:11 +0100

    Added tests

---
+++ src/Evaluation/RelativeForkEvaluation.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Chess\Evaluation;
+
+use Chess\Board;
+use Chess\PGN\Symbol;
+
+class RelativeForkEvaluation extends AbstractForkEvaluation
+{
+    const NAME = 'relative_fork';
+
+    public function __construct(Board $board)
+    {
+        parent::__construct($board);
+
+        $this->result = [
+            Symbol::WHITE => 0,
+            Symbol::BLACK => 0,
+        ];
+    }
+
+    public function evaluate(): array
+    {
+        foreach ($this->board->getPieces() as $piece) {
+            if ($piece->getIdentity() !== Symbol::KING) {
+                $attackedPieces = $this->attackedPieces($piece);
+                if (!$this->isKingAttacked($attackedPieces)) {
+                    $this->result[$piece->getColor()] = $this->sumValues($piece, $attackedPieces);
+                }
+            }
+        }
+
+        return $this->result;
+    }
+}
