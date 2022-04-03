6e7da3c124b71b85d1454c4ec0a5eaaeea3fb84c
programarivm <info@programarivm.com>
2022-04-03 12:08:46 +0200

    Created images and restructured the img folder

--- src/Media/BoardToPng.php
+++ src/Media/BoardToPng.php
@@ -11,19 +11,28 @@ class BoardToPng
 {
     const FILEPATH = __DIR__ . '/../../img';
 
+    const SIZES = [
+        480 => 65,
+        3000 => 375,
+    ];
+
     protected $board;
 
     protected $imagine;
 
     protected $flip;
 
-    public function __construct(Board $board, bool $flip = false)
+    protected $size;
+
+    public function __construct(Board $board, bool $flip = false, $size = 480)
     {
         $this->board = $board;
 
         $this->imagine = new Imagine();
 
         $this->flip = $flip;
+
+        $this->size = $size;
     }
 
     public function setBoard(Board $board)
@@ -35,20 +44,22 @@ class BoardToPng
 
     public function output(string $filepath, string $salt = '')
     {
-        $chessboard = $this->imagine->open(self::FILEPATH . '/chessboard_480.png');
+        $chessboard = $this->imagine->open(self::FILEPATH.'/chessboard/'.$this->size.'.png');
         $array = (new Ascii())->toArray($this->board, $this->flip);
         $x = $y = 0;
         foreach ($array as $i => $rank) {
             foreach ($rank as $j => $piece) {
                 if ($piece !== ' . ') {
                     $filename = trim($piece);
-                    $image = $this->imagine->open(self::FILEPATH . "/pieces/$filename.png");
+                    $image = $this->imagine->open(
+                        self::FILEPATH .'/pieces/png/'.self::SIZES[$this->size].'/'.$filename.'.png'
+                    );
                     $chessboard->paste($image, new Point($x, $y));
                 }
-                $x += 60;
+                $x += self::SIZES[$this->size];
             }
             $x = 0;
-            $y += 60;
+            $y += self::SIZES[$this->size];
         }
 
         if ($salt) {
