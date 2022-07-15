f1d90dad1b5c81652478682b154ba9dc94c97884
programarivm <info@programarivm.com>
2022-07-15 22:36:35 +0200

    Implemented bestMove()

--- src/UCI/Stockfish.php
+++ src/UCI/Stockfish.php
@@ -5,12 +5,35 @@ namespace Chess\UCI;
 /**
  * Stockfish.
  *
- * PHP wrapper for the Stockfish chess engine.  
+ * PHP wrapper for the Stockfish chess engine.
  *
  * @author Jordi Bassagañas
  * @license GPL
  */
 class Stockfish
 {
+    public function bestMove(int $seconds)
+    {
+        $descr = [
+            ['pipe', 'r'],
+            ['pipe', 'w'],
+        ];
 
+        $pipes = [];
+
+        $process = proc_open('stockfish', $descr, $pipes);
+
+        if (is_resource($process)) {
+            fwrite($pipes[0], "uci\n");
+            fwrite($pipes[0], "go infinite\n");
+            sleep($seconds);
+            fwrite($pipes[0], "stop\n");
+            fclose($pipes[0]);
+            while (!feof($pipes[1])) {
+                echo fgets($pipes[1]);
+            }
+            fclose($pipes[1]);
+            proc_close($process);
+        }
+    }
 }
--- tests/unit/UCI/StockfishTest.php
+++ tests/unit/UCI/StockfishTest.php
@@ -2,10 +2,20 @@
 
 namespace Chess\Tests\Unit\Piece;
 
-use Chess\Stockfish;
+use Chess\UCI\Stockfish;
 use Chess\Tests\AbstractUnitTestCase;
 
 class StockfishTest extends AbstractUnitTestCase
 {
+    /**
+     * @test
+     */
+    public function best_move()
+    {
+        $stockfish = new Stockfish();
 
+        $stockfish->bestMove(3);
+
+        $this->assertTrue(true);
+    }
 }
