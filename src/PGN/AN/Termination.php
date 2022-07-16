dc21bb3ef95f24ab59a01d8a7984584f98ddaf5e
Jordi Bassaganas <info@programarivm.com>
2022-07-16 16:08:56 +0200

    Update Termination.php

--- src/PGN/AN/Termination.php
+++ src/PGN/AN/Termination.php
@@ -16,7 +16,8 @@ class Termination extends AbstractNotation
     const BLACK_WINS = '0-1';
     const DRAW = '1/2-1/2';
     const UNKNOWN = '*';
-    const WHITE_WINS_EXTENDED_ASCII = '1–0';
-    const BLACK_WINDS_EXTENDED_ASCII = '0–1';
-    const DRAW_EXTENDED_ASCII = '½–½';
+    const WHITE_WINS_EXTENDED_ASCII_150 = '1–0';
+    const BLACK_WINDS_EXTENDED_ASCII_150 = '0–1';
+    const DRAW_EXTENDED_ASCII_150 = '1/2–1/2';
+    const DRAW_EXTENDED_ASCII_150_189 = '½–½';
 }
