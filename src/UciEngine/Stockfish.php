fec6a9580b6278365e8664013fb026975715762c
programarivm <info@programarivm.com>
2022-07-16 13:14:37 +0200

    Implemented fen()

--- src/UciEngine/Stockfish.php
+++ src/UciEngine/Stockfish.php
@@ -59,4 +59,31 @@ class Stockfish
 
         return $bestMove;
     }
+
+    public function fen(string $fen, int $seconds): string
+    {
+        $bestMove = $this->bestMove($fen, $seconds);
+        if ($bestMove !== '(none)') {
+            $process = proc_open(self::NAME, $this->descr, $this->pipes);
+            if (is_resource($process)) {
+                fwrite($this->pipes[0], "uci\n");
+                fwrite($this->pipes[0], "position fen $fen moves $bestMove\n");
+                fwrite($this->pipes[0], "go infinite\n");
+                sleep($seconds);
+                fwrite($this->pipes[0], "stop\n");
+                fwrite($this->pipes[0], "d\n");
+                fclose($this->pipes[0]);
+                while (!feof($this->pipes[1])) {
+                    $line = fgets($this->pipes[1]);
+                    if (str_starts_with($line, 'Fen: ')) {
+                        $fen = substr($line, 5);
+                    }
+                }
+                fclose($this->pipes[1]);
+                proc_close($process);
+            }
+        }
+
+        return $fen;
+    }
 }
--- tests/unit/UciEngine/StockfishTest.php
+++ tests/unit/UciEngine/StockfishTest.php
@@ -3,6 +3,7 @@
 namespace Chess\Tests\Unit\UciEngine;
 
 use Chess\Board;
+use Chess\FEN\StrToBoard;
 use Chess\UciEngine\Stockfish;
 use Chess\Tests\AbstractUnitTestCase;
 
@@ -11,15 +12,20 @@ class StockfishTest extends AbstractUnitTestCase
     /**
      * @test
      */
-    public function best_move_e4()
+    public function fen()
     {
         $board = new Board();
         $board->play('w', 'e4');
 
         $stockfish = new Stockfish($board);
+        $fen = $stockfish->fen($board->toFen(), 3);
 
-        $bestMove = $stockfish->bestMove($board->toFen(), 3);
+        $board = (new StrToBoard($fen))->create();
+        $board->play('w', 'Ke2');
 
-        $this->assertNotEmpty($bestMove);
+        $stockfish = new Stockfish($board);
+        $fen = $stockfish->fen($board->toFen(), 3);
+
+        $this->assertNotEmpty($fen);
     }
 }
