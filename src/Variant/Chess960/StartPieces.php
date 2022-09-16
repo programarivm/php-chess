c3fb2857cdd5c4c9c18b6a08db75c861b86d1dad
programarivm <info@programarivm.com>
2022-09-16 17:27:38 +0200

    Added pawns to the start position

--- src/Variant/Chess960/StartPieces.php
+++ src/Variant/Chess960/StartPieces.php
@@ -4,6 +4,7 @@ namespace Chess\Variant\Chess960;
 
 use Chess\PGN\AN\Color;
 use Chess\PGN\AN\Piece;
+use Chess\Piece\P;
 use Chess\Piece\RType;
 use Chess\Variant\Chess960\StartPosition;
 
@@ -37,6 +38,24 @@ class StartPieces
                 $this->startPieces[] =  new $className(Color::B, $bSq, RType::CASTLE_SHORT);
             }
         }
+
+        $this->startPieces[] = new P(Color::W, 'a2');
+        $this->startPieces[] = new P(Color::W, 'b2');
+        $this->startPieces[] = new P(Color::W, 'c2');
+        $this->startPieces[] = new P(Color::W, 'd2');
+        $this->startPieces[] = new P(Color::W, 'e2');
+        $this->startPieces[] = new P(Color::W, 'f2');
+        $this->startPieces[] = new P(Color::W, 'g2');
+        $this->startPieces[] = new P(Color::W, 'h2');
+
+        $this->startPieces[] = new P(Color::B, 'a7');
+        $this->startPieces[] = new P(Color::B, 'b7');
+        $this->startPieces[] = new P(Color::B, 'c7');
+        $this->startPieces[] = new P(Color::B, 'd7');
+        $this->startPieces[] = new P(Color::B, 'e7');
+        $this->startPieces[] = new P(Color::B, 'f7');
+        $this->startPieces[] = new P(Color::B, 'g7');
+        $this->startPieces[] = new P(Color::B, 'h7');
 
         return $this->startPieces;
     }
