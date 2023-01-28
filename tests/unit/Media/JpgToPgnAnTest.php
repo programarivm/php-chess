59009ec817ed59279abd72342aa34f5e8161aaeb
programarivm <info@programarivm.com>
2023-01-28 12:56:13 +0100

    Implemented Chess\Media\JpgToPgnAn

---
+++ tests/unit/Media/JpgToPgnAnTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Chess\Tests\Unit\Media;
+
+use Chess\Media\JpgToPgnAn;
+use Chess\Tests\AbstractUnitTestCase;
+
+class JpgToPgnAnTest extends AbstractUnitTestCase
+{
+    /**
+     * @test
+     */
+    public function predict_w_B()
+    {
+        $filename = self::DATA_FOLDER.'/img/B.jpg';
+
+        $expected = 'B';
+
+        $this->assertSame($expected, (new JpgToPgnAn($filename))->predict());
+    }
+
+    /**
+     * @test
+     */
+    public function predict_b_B()
+    {
+        $filename = self::DATA_FOLDER.'/img/b.jpg';
+
+        $expected = 'b';
+
+        $this->assertSame($expected, (new JpgToPgnAn($filename))->predict());
+    }
+
+    /**
+     * @test
+     */
+    public function predict_w_R()
+    {
+        $filename = self::DATA_FOLDER.'/img/R.jpg';
+
+        $expected = 'R';
+
+        $this->assertSame($expected, (new JpgToPgnAn($filename))->predict());
+    }
+
+    /**
+     * @test
+     */
+    public function predict_b_R()
+    {
+        $filename = self::DATA_FOLDER.'/img/r.jpg';
+
+        $expected = 'r';
+
+        $this->assertSame($expected, (new JpgToPgnAn($filename))->predict());
+    }
+}
