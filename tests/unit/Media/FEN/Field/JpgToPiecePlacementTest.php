7eef01d0dbeff683df35db4b61d3e879ced09135
programarivm <info@programarivm.com>
2023-02-04 13:42:33 +0100

    Added test

--- tests/unit/Media/FEN/Field/JpgToPiecePlacementTest.php
+++ tests/unit/Media/FEN/Field/JpgToPiecePlacementTest.php
@@ -18,4 +18,16 @@ class JpgToPiecePlacementTest extends AbstractUnitTestCase
 
         $this->assertSame($expected, (new JpgToPiecePlacement($filename))->predict());
     }
+
+    /**
+     * @test
+     */
+    public function predict_02_kaufman()
+    {
+        $filename = self::DATA_FOLDER.'/img/02_kaufman.jpg';
+
+        $expected = '3r2k1/p2r1p1p/1p2p1p1/q4n2/3P4/PQ5P/1P1RNPP1/3R2K1';
+
+        $this->assertSame($expected, (new JpgToPiecePlacement($filename))->predict());
+    }
 }
