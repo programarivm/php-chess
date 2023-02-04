1142d04874e099b190b23e4c962af99eb0f55aaf
programarivm <info@programarivm.com>
2023-02-04 11:49:08 +0100

    Implemented constructor

--- src/Media/FEN/Field/JpgToPiecePlacement.php
+++ src/Media/FEN/Field/JpgToPiecePlacement.php
@@ -2,8 +2,17 @@
 
 namespace Chess\Media\FEN\Field;
 
+use Chess\Media\PGN\AN\JpgToPiece;
+
 class JpgToPiecePlacement
 {
+    protected string $filename;
+
+    public function __construct(string $filename)
+    {
+        $this->filename = $filename;
+    }
+
     public function predict(): string
     {
         // TODO ...
