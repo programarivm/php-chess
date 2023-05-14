b0f9ca5315301c342b0cabcfdcf9e1147ce7de68
programarivm <info@programarivm.com>
2023-05-14 12:55:31 +0200

    Refactored Chess\Variant\Classical\Board

--- src/Variant/Classical/Board.php
+++ src/Variant/Classical/Board.php
@@ -1015,43 +1015,49 @@ class Board extends \SplObjectStorage
                                 $piece->sqCastleShort() &&
                                 $clone->play($color, Castle::SHORT)
                             ) {
-                                $fen[$sq] = $clone->toFen();
+                                $fen[$sq] = $clone->getHistory()[count($clone->getHistory()) - 1]->fen;
                             } elseif (
                                 $this->castlingRule[$color][Piece::K][Castle::LONG]['sq']['next'] === $sq &&
                                 $piece->sqCastleLong() &&
                                 $clone->play($color, Castle::LONG)
                             ) {
-                                $fen[$sq] = $clone->toFen();
+                                $fen[$sq] = $clone->getHistory()[count($clone->getHistory()) - 1]->fen;
                             } elseif ($clone->play($color, Piece::K.'x'.$sq)) {
-                                $fen[$sq] = $clone->toFen();
+                                $fen[$sq] = $clone->getHistory()[count($clone->getHistory()) - 1]->fen;
                             } elseif ($clone->play($color, Piece::K.$sq)) {
-                                $fen[$sq] = $clone->toFen();
+                                $fen[$sq] = $clone->getHistory()[count($clone->getHistory()) - 1]->fen;
                             }
                             break;
                         case Piece::P:
                             if ($clone->play($color, $piece->getSqFile()."x$sq")) {
-                                $fen[$sq] = $clone->toFen();
+                                $fen[$sq] = $clone->getHistory()[count($clone->getHistory()) - 1]->fen;
                             } elseif ($clone->play($color, $sq)) {
-                                $fen[$sq] = $clone->toFen();
+                                $fen[$sq] = $clone->getHistory()[count($clone->getHistory()) - 1]->fen;
                             }
                             break;
                         default:
                             if ($clone->play($color, "{$piece->getId()}x$sq")) {
-                                $fen[$sq] = $clone->toFen();
+                                $fen[$sq] = $clone->getHistory()[count($clone->getHistory()) - 1]->fen;
                             } elseif ($clone->play($color, $piece->getId().$sq)) {
-                                $fen[$sq] = $clone->toFen();
+                                $fen[$sq] = $clone->getHistory()[count($clone->getHistory()) - 1]->fen;
                             } elseif ($clone->play($color, "{$piece->getId()}{$piece->getSqFile()}x$sq")) {
-                                $fen[$sq] = $clone->toFen(); // disambiguation by file
+                                // disambiguation by file
+                                $fen[$sq] = $clone->getHistory()[count($clone->getHistory()) - 1]->fen;
                             } elseif ($clone->play($color, $piece->getId().$piece->getSqFile().$sq)) {
-                                $fen[$sq] = $clone->toFen(); // disambiguation by file
+                                // disambiguation by file
+                                $fen[$sq] = $clone->getHistory()[count($clone->getHistory()) - 1]->fen;
                             } elseif ($clone->play($color, "{$piece->getId()}{$piece->getSqRank()}x$sq")) {
-                                $fen[$sq] = $clone->toFen(); // disambiguation by rank
+                                // disambiguation by rank
+                                $fen[$sq] = $clone->getHistory()[count($clone->getHistory()) - 1]->fen;
                             } elseif ($clone->play($color, $piece->getId().$piece->getSqRank().$sq)) {
-                                $fen[$sq] = $clone->toFen(); // disambiguation by rank
+                                // disambiguation by rank
+                                $fen[$sq] = $clone->getHistory()[count($clone->getHistory()) - 1]->fen;
                             } elseif ($clone->play($color, "{$piece->getId()}{$piece->getSq()}x$sq")) {
-                                $fen[$sq] = $clone->toFen(); // disambiguation by square
+                                // disambiguation by square
+                                $fen[$sq] = $clone->getHistory()[count($clone->getHistory()) - 1]->fen;
                             } elseif ($clone->play($color, $piece->getId().$piece->getSq().$sq)) {
-                                $fen[$sq] = $clone->toFen(); // disambiguation by square
+                                // disambiguation by square
+                                $fen[$sq] = $clone->getHistory()[count($clone->getHistory()) - 1]->fen;
                             }
                         break;
                     }
