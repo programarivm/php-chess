aa3de6eab8daa9a3f0d7c6045f3646a5a475dc58
programarivm <info@programarivm.com>
2023-07-12 13:06:14 +0200

    Fixed filtered()

--- src/Movetext/SanMovetext.php
+++ src/Movetext/SanMovetext.php
@@ -258,6 +258,9 @@ class SanMovetext extends AbstractMovetext
         if (!$nags) {
             // remove nags
             preg_match_all('/\$[1-9][0-9]*/', $str, $matches);
+            usort($matches[0], function($a, $b) {
+                return strlen($a) < strlen($b);
+            });
             foreach (array_filter($matches[0]) as $match) {
                 $str = str_replace($match, '', $str);
             }
