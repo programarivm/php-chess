a2c83e324976e66d5b08efd59b2155e158eb96c6
programarivm <info@programarivm.com>
2023-12-24 12:01:00 +0100

    Implemented Chess\Eval\ThreatEval

--- tests/unit/Eval/ThreatEvalTest.php
+++ tests/unit/Eval/ThreatEvalTest.php
@@ -14,15 +14,15 @@ class ThreatEvalTest extends AbstractUnitTestCase
     public function endgame()
     {
         $expectedResult = [
-            'w' => 0,
-            'b' => 1,
+            'w' => 1,
+            'b' => 0,
         ];
 
         $expectedPhrase = [
             "The pawn on d4 is being threatened.",
         ];
 
-        $board = (new StrToBoard('6k1/6p1/2k2b2/8/3P4/5N2/2K5/8 w - -'))->create();
+        $board = (new StrToBoard('6k1/6p1/2n2b2/8/3P4/5N2/2K5/8 w - -'))->create();
         $threatEval = new ThreatEval($board);
 
         $this->assertSame($expectedResult, $threatEval->getResult());
