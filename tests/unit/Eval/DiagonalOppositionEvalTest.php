ebf53c628c6c2c8bcfdfc6ebe3b3f185a1ce577a
programarivm <info@programarivm.com>
2024-01-04 16:09:56 +0100

    Added test

--- tests/unit/Eval/DiagonalOppositionEvalTest.php
+++ tests/unit/Eval/DiagonalOppositionEvalTest.php
@@ -22,8 +22,28 @@ class DiagonalOppositionEvalTest extends AbstractUnitTestCase
             "The white king has the diagonal opposition preventing the advance of the other king.",
         ];
 
-        $fen = '8/8/2K5/8/4k3/8/8/8 b - - 0 1';
-        $board = (new StrToBoard($fen))->create();
+        $board = (new StrToBoard('8/8/2K5/8/4k3/8/8/8 b - - 0 1'))->create();
+        $directOppositionEval = new DiagonalOppositionEval($board);
+
+        $this->assertSame($expectedResult, $directOppositionEval->getResult());
+        $this->assertSame($expectedPhrase, $directOppositionEval->getPhrases());
+    }
+
+    /**
+     * @test
+     */
+    public function position_02()
+    {
+        $expectedResult = [
+            'w' => 1,
+            'b' => 0,
+        ];
+
+        $expectedPhrase = [
+            "The white king has the diagonal opposition preventing the advance of the other king.",
+        ];
+
+        $board = (new StrToBoard('5k2/6p1/3K2P1/5P2/8/8/8/8 b - - 0 1'))->create();
         $directOppositionEval = new DiagonalOppositionEval($board);
 
         $this->assertSame($expectedResult, $directOppositionEval->getResult());
