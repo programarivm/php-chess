1148a5fd08be77f08cb3d4a7ad066b8495e837ed
programarivm <info@programarivm.com>
2024-02-27 13:14:58 +0100

    Updated tests

--- tests/unit/UciEngine/StockfishTest.php
+++ tests/unit/UciEngine/StockfishTest.php
@@ -12,16 +12,6 @@ class StockfishTest extends AbstractUnitTestCase
     /**
      * @test
      */
-    public function instantiation()
-    {
-        $stockfish = new Stockfish('/usr/games/stockfish');
-
-        $this->assertTrue(is_a($stockfish, Stockfish::class));
-    }
-
-    /**
-     * @test
-     */
     public function get_options()
     {
         $stockfish = new Stockfish('/usr/games/stockfish');
@@ -67,46 +57,6 @@ class StockfishTest extends AbstractUnitTestCase
         $limit->time = 3000;
 
         $stockfish = new Stockfish('/usr/games/stockfish');
-
-        $expected = 'c7c5';
-        $analysis = $stockfish->analyse($board, $limit);
-
-        $this->assertSame($expected, $analysis['bestmove']);
-    }
-
-    /**
-     * @test
-     */
-    public function analyse_e4_skill_level_17_limit_time_300()
-    {
-        $board = new Board();
-        $board->play('w', 'e4');
-
-        $limit = new Limit();
-        $limit->time = 3000;
-
-        $stockfish = new Stockfish('/usr/games/stockfish');
-        $stockfish->setOption('Skill Level', 17);
-
-        $expected = 'c7c5';
-        $analysis = $stockfish->analyse($board, $limit);
-
-        $this->assertSame($expected, $analysis['bestmove']);
-    }
-
-    /**
-     * @test
-     */
-    public function analyse_e4_skill_level_20_depth_8()
-    {
-        $board = new Board();
-        $board->play('w', 'e4');
-
-        $limit = new Limit();
-        $limit->depth = 8;
-
-        $stockfish = new Stockfish('/usr/games/stockfish');
-        $stockfish->setOption('Skill Level', 20);
 
         $expected = 'c7c5';
         $analysis = $stockfish->analyse($board, $limit);
