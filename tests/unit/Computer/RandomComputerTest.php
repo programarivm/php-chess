3416b5523f2dfb7444b9faff8e733ef03c8e5c43
programarivm <info@programarivm.com>
2024-04-29 10:55:09 +0200

    Added tests

--- tests/unit/Computer/RandomComputerTest.php
+++ tests/unit/Computer/RandomComputerTest.php
@@ -3,6 +3,7 @@
 namespace Chess\Tests\Unit\Computer;
 
 use Chess\Computer\RandomComputer;
+use Chess\Play\SanPlay;
 use Chess\Tests\AbstractUnitTestCase;
 use Chess\Variant\Classical\Board;
 
@@ -17,5 +18,29 @@ class RandomComputerTest extends AbstractUnitTestCase
         $randomComputer = new RandomComputer();
 
         $this->assertNotEmpty($randomComputer->move($board));
+    }
+
+    /**
+     * @test
+     */
+    public function scholars_mate_4_w()
+    {
+        $movetext = '1.e4 e5 2.Qh5 Nc6 3.Bc4 Nf6';
+        $board = (new SanPlay($movetext))->validate()->getBoard();
+        $randomComputer = new RandomComputer();
+
+        $this->assertNotEmpty($randomComputer->move($board));
+    }
+
+    /**
+     * @test
+     */
+    public function scholars_mate_4_b()
+    {
+        $movetext = '1.e4 e5 2.Qh5 Nc6 3.Bc4 Nf6 Qxf7#';
+        $board = (new SanPlay($movetext))->validate()->getBoard();
+        $randomComputer = new RandomComputer();
+
+        $this->assertSame(null, $randomComputer->move($board));
     }
 }
