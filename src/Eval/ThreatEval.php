fad5e2100523b0a8423e2872db33f7195bb39a81
programarivm <info@programarivm.com>
2024-05-24 16:33:52 +0200

    Fixed Chess\Eval\ThreatEval

--- src/Eval/ThreatEval.php
+++ src/Eval/ThreatEval.php
@@ -5,6 +5,7 @@ namespace Chess\Eval;
 use Chess\Piece\AbstractPiece;
 use Chess\Variant\Classical\Board;
 use Chess\Variant\Classical\PGN\AN\Color;
+use Chess\Variant\Classical\PGN\AN\Piece;
 
 class ThreatEval extends AbstractEval implements
     ElaborateEvalInterface,
@@ -32,41 +33,39 @@ class ThreatEval extends AbstractEval implements
             "has a total threat advantage",
         ];
 
-        foreach ($this->board->getPieces() as $piece) {
-            $clone = unserialize(serialize($this->board));
-            $clone->setTurn($piece->oppColor());
-
-            $threat = [
-                Color::W => 0,
-                Color::B => 0,
-            ];
-
-            $attackingPiece = current($piece->attackingPieces());
-            while ($attackingPiece) {
-                $capturedPiece = $clone->getPieceBySq($piece->getSq());
-                if ($clone->playLan($clone->getTurn(), $attackingPiece->getSq() . $piece->getSq())) {
-                    $threat[$attackingPiece->getColor()] += self::$value[$capturedPiece->getId()];
-                    if ($defendingPiece = current($piece->defendingPieces())) {
-                        $capturedPiece = $clone->getPieceBySq($piece->getSq());
-                        if ($clone->playLan($clone->getTurn(), $defendingPiece->getSq() . $piece->getSq())) {
-                            $threat[$defendingPiece->getColor()] += self::$value[$capturedPiece->getId()];
+        if (!$this->board->isMate()) {
+            foreach ($this->board->getPieces() as $piece) {
+                $clone = unserialize(serialize($this->board));
+                $clone->setTurn($piece->oppColor());
+                $threat = [
+                    Color::W => 0,
+                    Color::B => 0,
+                ];
+                $attackingPiece = current($piece->attackingPieces());
+                while ($attackingPiece && $piece->getId() !== Piece::K) {
+                    $capturedPiece = $clone->getPieceBySq($piece->getSq());
+                    if ($clone->playLan($clone->getTurn(), $attackingPiece->getSq() . $piece->getSq())) {
+                        $threat[$attackingPiece->getColor()] += self::$value[$capturedPiece->getId()];
+                        if ($defendingPiece = current($piece->defendingPieces())) {
+                            $capturedPiece = $clone->getPieceBySq($piece->getSq());
+                            if ($clone->playLan($clone->getTurn(), $defendingPiece->getSq() . $piece->getSq())) {
+                                $threat[$defendingPiece->getColor()] += self::$value[$capturedPiece->getId()];
+                            }
                         }
+                        $attackingPiece = current($clone->getPieceBySq($piece->getSq())->attackingPieces());
                     }
-                    $attackingPiece = current($clone->getPieceBySq($piece->getSq())->attackingPieces());
                 }
-            }
-
-            $diff = $threat[Color::W] - $threat[Color::B];
-
-            if ($piece->oppColor() === Color::W) {
-                if ($diff > 0) {
-                    $this->result[Color::W] += $diff;
-                    $this->elaborate($piece);
-                }
-            } else {
-                if ($diff < 0) {
-                    $this->result[Color::B] += abs($diff);
-                    $this->elaborate($piece);
+                $diff = $threat[Color::W] - $threat[Color::B];
+                if ($piece->oppColor() === Color::W) {
+                    if ($diff > 0) {
+                        $this->result[Color::W] += $diff;
+                        $this->elaborate($piece);
+                    }
+                } else {
+                    if ($diff < 0) {
+                        $this->result[Color::B] += abs($diff);
+                        $this->elaborate($piece);
+                    }
                 }
             }
         }
