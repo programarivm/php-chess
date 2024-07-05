0bcb42e7d649063c3b88a80876f49d6e683105d8
programarivm <info@programarivm.com>
2024-07-05 13:15:36 +0200

    Added test

--- tests/unit/Variant/Dunsany/BoardTest.php
+++ tests/unit/Variant/Dunsany/BoardTest.php
@@ -16,4 +16,25 @@ class BoardTest extends AbstractUnitTestCase
 
         $this->assertSame(48, count($board->pieces()));
     }
+
+    /**
+     * @test
+     */
+    public function start()
+    {
+        $board = new Board();
+
+        $expected = [
+            7 => [ ' r ', ' n ', ' b ', ' q ', ' k ', ' b ', ' n ', ' r ' ],
+            6 => [ ' p ', ' p ', ' p ', ' p ', ' p ', ' p ', ' p ', ' p ' ],
+            5 => [ ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ' ],
+            4 => [ ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ' ],
+            3 => [ ' P ', ' P ', ' P ', ' P ', ' P ', ' P ', ' P ', ' P ' ],
+            2 => [ ' P ', ' P ', ' P ', ' P ', ' P ', ' P ', ' P ', ' P ' ],
+            1 => [ ' P ', ' P ', ' P ', ' P ', ' P ', ' P ', ' P ', ' P ' ],
+            0 => [ ' P ', ' P ', ' P ', ' P ', ' P ', ' P ', ' P ', ' P ' ],
+        ];
+
+        $this->assertSame($expected, $board->toAsciiArray());
+    }
 }
