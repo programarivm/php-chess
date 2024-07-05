03f0acc0670eabf902fe9196358c2dc30a4584fc
programarivm <info@programarivm.com>
2024-07-05 15:00:58 +0200

    Implemented Chess\Variant\Dunsany\FEN

--- tests/unit/Variant/Dunsany/BoardTest.php
+++ tests/unit/Variant/Dunsany/BoardTest.php
@@ -38,4 +38,27 @@ class BoardTest extends AbstractUnitTestCase
         $this->assertSame($expected, $board->toAsciiArray());
         $this->assertSame('b', $board->turn);
     }
+
+    /**
+     * @test
+     */
+    public function play_e6()
+    {
+        $board = new Board();
+
+        $board->play('b', 'e5');
+
+        $expected = [
+            7 => [ ' r ', ' n ', ' b ', ' q ', ' k ', ' b ', ' n ', ' r ' ],
+            6 => [ ' p ', ' p ', ' p ', ' p ', ' . ', ' p ', ' p ', ' p ' ],
+            5 => [ ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ' ],
+            4 => [ ' . ', ' . ', ' . ', ' . ', ' p ', ' . ', ' . ', ' . ' ],
+            3 => [ ' P ', ' P ', ' P ', ' P ', ' P ', ' P ', ' P ', ' P ' ],
+            2 => [ ' P ', ' P ', ' P ', ' P ', ' P ', ' P ', ' P ', ' P ' ],
+            1 => [ ' P ', ' P ', ' P ', ' P ', ' P ', ' P ', ' P ', ' P ' ],
+            0 => [ ' P ', ' P ', ' P ', ' P ', ' P ', ' P ', ' P ', ' P ' ],
+        ];
+
+        $this->assertSame($expected, $board->toAsciiArray());
+    }
 }
