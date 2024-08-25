fa97ceb4c66fde3a773041a8f74590c4d0e860e3
Kartik Arora <arorakartik4@gmail.com>
2024-08-25 17:59:42 +0530

    tdd for `checkExposedPieceAdvantage`

--- src/Eval/DeflectionEval.php
+++ src/Eval/DeflectionEval.php
@@ -5,9 +5,8 @@ namespace Chess\Eval;
 use Chess\Tutor\PiecePhrase;
 use Chess\Variant\AbstractBoard;
 use Chess\Variant\AbstractPiece;
-use Chess\Variant\Classical\PGN\AN\Piece;
 
-class DeflectionEval extends AbstractEval implements 
+class DeflectionEval extends AbstractEval implements
     ElaborateEvalInterface,
     ExplainEvalInterface
 {
@@ -16,18 +15,110 @@ class DeflectionEval extends AbstractEval implements
 
     const NAME = 'Deflection';
 
+    protected bool $deflectionExists = false;
+
     public function __construct(AbstractBoard $board)
     {
         $this->board = $board;
-
         
+        $this->range = [1, 6];
+
+        $this->subject = [
+            'White',
+            'Black',
+        ];
+
+        $this->observation = [
+            "has a slight deflection advantage",
+            "has a moderate deflection advantage",
+            "has a total deflection advantage",
+        ];
+
+        foreach ($this->board->pieces() as $piece) {
+            if ($piece->color === $this->board->turn) {
+                $legalMoveSquares = $board->legal($piece->sq);
+                $attackingPieces = $piece->attacking();
+
+                if (!empty($attackingPieces) && !empty($legalMoveSquares)) {
+                    foreach ($legalMoveSquares as $square) {
+                        $clone = $this->board->clone();
+                        $clone->playLan($clone->turn, $piece->sq . $square);
+                        $clone->refresh();
+
+                        $primaryDeflectionPhrase = $this->primaryPhrase($piece, $attackingPieces);
+
+                        $this->checkExposedPieceAdvantage($clone, $piece, $primaryDeflectionPhrase);
+
+                        if ($this->deflectionExists) {
+                            break;
+                        }
+                    }
+                }
+            }
+            if ($this->deflectionExists) {
+                break;
+            }
+        }
+
+        $this->explain($this->result);
     }
 
-    // private function elaborate(AbstractPiece $attacking, AbstractPiece $attacked): void
-    // {
-    //     $attacking = PiecePhrase::create($attacking);
-    //     $attacked = PiecePhrase::create($attacked);
+    private function primaryPhrase(AbstractPiece $piece, array $attackingPieces): string {
+        $piecePhrase = PiecePhrase::create($piece);
 
-    //     $this->elaboration[] = ucfirst("when $attacked will be moved, a piece that is more valuable than $attacking TODO.");
-    // }
+        $attackedByPhrase = $this->attackedByPhrase($attackingPieces);
+
+        return "$piecePhrase is deflected due to $attackedByPhrase, ";
+    }
+
+    private function attackedByPhrase(array $attackingPieces): string {
+        if (count($attackingPieces) === 1) {
+            return PiecePhrase::create($attackingPieces[0]);
+        } elseif (count($attackingPieces) > 1) {
+            $squares = array_map(fn($attacking) => PiecePhrase::create($attacking), $attackingPieces);
+            $lastSquare = array_pop($squares);
+            return implode(', ', $squares) . ' and ' . $lastSquare;
+        }
+    }
+
+    private function checkExposedPieceAdvantage(AbstractBoard $clone, AbstractPiece $piece, string $primaryDeflectionPhrase) {
+        $diffPhrases = [];
+        $protectionEval = new ProtectionEval($this->board);
+        $newProtectionEval = new ProtectionEval($clone);
+        $diffResult = $newProtectionEval->getResult()[$piece->oppColor()]
+            - $protectionEval->getResult()[$piece->oppColor()];
+        if ($diffResult > 0) {
+            foreach ($newProtectionEval->getElaboration() as $key => $val) {
+                if (!in_array($val, $protectionEval->getElaboration())) {
+                    $diffPhrases[] = $val;
+                }
+            }
+            $this->result[$piece->oppColor()] += round($diffResult, 2);
+            $this->elaborateExposedPieceAdvantage($primaryDeflectionPhrase, $diffPhrases);
+            $this->deflectionExists = true;
+        }
+    }
+
+    private function elaborateExposedPieceAdvantage(String $phrase, array $diffPhrases): void
+    {
+        $count = count($diffPhrases);
+        if ($count === 1) {
+            $diffPhrase = mb_strtolower($diffPhrases[0]);
+            $rephrase = str_replace('is unprotected', 'may well be exposed to attack', $diffPhrase);
+            $phrase .= $rephrase;
+        } elseif ($count > 1) {
+            $phrase .= 'these pieces may well be exposed to attack: ';
+            $rephrase = '';
+            foreach ($diffPhrases as $diffPhrase) {
+                $rephrase .= str_replace(' is unprotected.', ', ', $diffPhrase);
+            }
+            $phrase .= $rephrase;
+            $phrase = str_replace(', The', ', the', $phrase);
+            $phrase = substr_replace(trim($phrase), '.', -1);
+        }
+
+        $this->elaboration[] = $phrase;
+
+        // $this->elaboration[] = substr_replace($phrase, ';', -1);
+    }
 }
--- tests/unit/Eval/DeflectionEvalTest.php
+++ tests/unit/Eval/DeflectionEvalTest.php
@@ -11,7 +11,7 @@ class DeflectionEvalTest extends AbstractUnitTestCase
     /**
      * @test
      */
-    public function basic_test()
+    public function basic_deflection_test()
     {
         $expectedResult = [
           'w' => 8.8,
@@ -23,7 +23,7 @@ class DeflectionEvalTest extends AbstractUnitTestCase
         ];
 
         $expectedElaboration = [
-          "If Black's king on e8 is deflected due to the bishop on f7, black's queen on d8 may well be exposed to attack.",
+          "Black's king on e8 is deflected due to the bishop on f7, black's queen on d8 may well be exposed to attack.",
         ];
 
         $board = (new StrToBoard('rnbqkb1r/pp2pBpp/5n2/8/4P3/2N5/PPP2PPP/R1BQK2R b - - 0 1'))
@@ -34,6 +34,108 @@ class DeflectionEvalTest extends AbstractUnitTestCase
         $this->assertSame($expectedResult, $deflectionEval->getResult());
         $this->assertSame($expectedExplanation, $deflectionEval->getExplanation());
         $this->assertSame($expectedElaboration, $deflectionEval->getElaboration());
+    }
+
+    /**
+     * @test
+     */
+    public function deflection_with_multiple_pieces()
+    {
+        $expectedResult = [
+          'w' => 8.8,
+          'b' => 0,
+        ];
+
+        $expectedExplanation = [
+          "White has a total deflection advantage.",
+        ];
+
+        $expectedElaboration = [
+          "Black's king on e8 is deflected due to the bishop on f7 and the rook on e1, black's queen on d8 may well be exposed to attack.",
+        ];
+
+        $board = (new StrToBoard('rnbqkb1r/pp3Bpp/5n2/8/8/2N5/PPP2PPP/R1BQR1K1 b - - 0 1'))
+          ->create();
+
+        $deflectionEval = new DeflectionEval($board);
+
+        $this->assertSame($expectedResult, $deflectionEval->getResult());
+        $this->assertSame($expectedExplanation, $deflectionEval->getExplanation());
+        $this->assertSame($expectedElaboration, $deflectionEval->getElaboration());
+    }
+
+    /**
+     * @test
+     */
+    public function moderate_deflection_advantage()
+    {
+        $expectedResult = [
+          'w' => 3.33,
+          'b' => 0,
+        ];
+
+        $expectedExplanation = [
+          "White has a moderate deflection advantage.",
+        ];
+
+        $expectedElaboration = [
+          "Black's king on e8 is deflected due to the bishop on f7, the bishop on d7 may well be exposed to attack.",
+        ];
+
+        $board = (new StrToBoard('4kb1r/pp1bpB1p/8/n7/8/2N5/PPP2PPP/3R1RK1 b - - 0 1'))
+          ->create();
+
+        $deflectionEval = new DeflectionEval($board);
+
+        $this->assertSame($expectedResult, $deflectionEval->getResult());
+        $this->assertSame($expectedExplanation, $deflectionEval->getExplanation());
+        $this->assertSame($expectedElaboration, $deflectionEval->getElaboration());
+    }
+
+    /**
+     * @test
+     */
+    public function slight_deflection_advantage()
+    {
+        $expectedResult = [
+          'w' => 1.0,
+          'b' => 0,
+        ];
+
+        $expectedExplanation = [
+          "White has a slight deflection advantage.",
+        ];
+
+        $expectedElaboration = [
+          "Black's king on e8 is deflected due to the bishop on f7, the pawn on d7 may well be exposed to attack.",
+        ];
+
+        $board = (new StrToBoard('4kb1r/p2ppB1p/8/n7/8/2N5/PPP2PPP/3R1RK1 b - - 0 1'))
+          ->create();
+
+        $deflectionEval = new DeflectionEval($board);
+
+        $this->assertSame($expectedResult, $deflectionEval->getResult());
+        $this->assertSame($expectedExplanation, $deflectionEval->getExplanation());
+        $this->assertSame($expectedElaboration, $deflectionEval->getElaboration());
+    }
+
+    /**
+     * @test
+     */
+    public function dev_test()
+    {
+        $board = (new StrToBoard('4kb1r/p2ppB1p/8/n7/8/2N5/PPP2PPP/3R1RK1 b - - 0 1'))
+          ->create();
+
+        $deflectionEval = new DeflectionEval($board);
+
+        print_r("*****DEV_TEST*****");
+        print_r($deflectionEval->getResult());
+        print_r($deflectionEval->getExplanation());
+        print_r($deflectionEval->getElaboration());
+        print_r("*****DEV_TEST*****");
+        $this->assertSame(true, true);
     }
 
 }
