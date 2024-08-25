b5357f3ad85b5dad7f9f0c5a0fbb85de2a661dad
Kartik Arora <arorakartik4@gmail.com>
2024-08-25 16:41:22 +0530

    basic test case for deflection

--- tests/unit/Eval/DeflectionEvalTest.php
+++ tests/unit/Eval/DeflectionEvalTest.php
@@ -3,10 +3,8 @@
 namespace Chess\Tests\Unit\Eval;
 
 use Chess\Eval\DeflectionEval;
-use Chess\Eval\RelativePinEval;
 use Chess\Variant\Classical\FEN\StrToBoard;
 use Chess\Tests\AbstractUnitTestCase;
-use Chess\Variant\Classical\Board;
 
 class DeflectionEvalTest extends AbstractUnitTestCase
 {
@@ -15,7 +13,27 @@ class DeflectionEvalTest extends AbstractUnitTestCase
      */
     public function basic_test()
     {
-        
+        $expectedResult = [
+          'w' => 8.8,
+          'b' => 0,
+        ];
+
+        $expectedExplanation = [
+          "White has a total deflection advantage.",
+        ];
+
+        $expectedElaboration = [
+          "If Black's king on e8 is deflected due to the bishop on f7, black's queen on d8 may well be exposed to attack.",
+        ];
+
+        $board = (new StrToBoard('rnbqkb1r/pp2pBpp/5n2/8/4P3/2N5/PPP2PPP/R1BQK2R b - - 0 1'))
+          ->create();
+
+        $deflectionEval = new DeflectionEval($board);
+
+        $this->assertSame($expectedResult, $deflectionEval->getResult());
+        $this->assertSame($expectedExplanation, $deflectionEval->getExplanation());
+        $this->assertSame($expectedElaboration, $deflectionEval->getElaboration());
     }
 
 }
