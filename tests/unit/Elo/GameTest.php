cedbbcf5b92ff9b5e17b9da1d8bcf73c56884365
programarivm <info@programarivm.com>
2024-09-06 17:36:22 +0200

    Updated GameTest

--- tests/unit/Elo/GameTest.php
+++ tests/unit/Elo/GameTest.php
@@ -17,10 +17,11 @@ class GameTest extends AbstractUnitTestCase
         $b = new Player(1400);
 
         $game =  new Game($w, $b);
-        $game->setK(32);
-        $game->setScore(1, 0)->count();
+        $game->setK(32)
+            ->setScore(1, 0)
+            ->count();
 
-        $this->assertGreaterThan(1400, $w->getRating());
-        $this->assertLessThan(1400, $b->getRating());
+        $this->assertEquals(1416, $w->getRating());
+        $this->assertEquals(1384, $b->getRating());
     }
 }
