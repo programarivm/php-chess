23f64609cee37b7e992123355de266ddf9a08a9b
programarivm <info@programarivm.com>
2024-10-27 11:34:39 +0100

    Added test

--- tests/unit/Media/FEN/ImgToPiecePlacementTest.php
+++ tests/unit/Media/FEN/ImgToPiecePlacementTest.php
@@ -10,11 +10,23 @@ class ImgToPiecePlacementTest extends AbstractUnitTestCase
     /**
      * @test
      */
-    public function predict_01_kaufman()
+    public function predict_01_kaufman_jpg()
     {
         $expected = '1rbq1rk1/p1b1nppp/1p2p3/8/1B1pN3/P2B4/1P3PPP/2RQ1R1K';
 
         $image = imagecreatefromjpeg(self::DATA_FOLDER.'/img/01_kaufman.jpg');
+
+        $this->assertSame($expected, (new ImgToPiecePlacement($image))->predict());
+    }
+
+    /**
+     * @test
+     */
+    public function predict_01_kaufman_png()
+    {
+        $expected = '1rbq1rk1/p1b1nppp/1p2p3/8/1B1pN3/P2B4/1P3PPP/2RQ1R1K';
+
+        $image = imagecreatefrompng(self::DATA_FOLDER.'/img/01_kaufman.png');
 
         $this->assertSame($expected, (new ImgToPiecePlacement($image))->predict());
     }
