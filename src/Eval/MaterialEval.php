57aacea344fdc21be2f09cfe591b7317b18c9341
programarivm <info@programarivm.com>
2024-11-02 11:20:17 +0100

    Documented MaterialEval

--- src/Eval/MaterialEval.php
+++ src/Eval/MaterialEval.php
@@ -6,12 +6,30 @@ use Chess\Variant\AbstractBoard;
 use Chess\Variant\Classical\PGN\AN\Color;
 use Chess\Variant\Classical\PGN\AN\Piece;
 
+/**
+ * Material Evaluation
+ *
+ * The player with the most material points has an advantage. The relative
+ * values of the pieces are assigned this way: 1 point to a pawn, 3.2 points to
+ * a knight, 3.33 points to a bishop, 5.1 points to a rook and 8.8 points to a
+ * queen.
+ *
+ * @see \Chess\Eval\AbstractEval
+ */
 class MaterialEval extends AbstractEval implements ExplainEvalInterface
 {
     use ExplainEvalTrait;
 
+    /**
+     * The name of the heuristic.
+     *
+     * @var string
+     */
     const NAME = 'Material';
 
+    /**
+     * @param \Chess\Variant\AbstractBoard $board
+     */
     public function __construct(AbstractBoard $board)
     {
         $this->board = $board;
