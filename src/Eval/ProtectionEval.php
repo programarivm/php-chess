94104906900509833f6b68ef667e22daa966202d
programarivm <info@programarivm.com>
2024-11-03 12:22:17 +0100

    Documented ProtectionEval

--- src/Eval/ProtectionEval.php
+++ src/Eval/ProtectionEval.php
@@ -5,9 +5,20 @@ namespace Chess\Eval;
 use Chess\Tutor\PiecePhrase;
 use Chess\Variant\AbstractBoard;
 use Chess\Variant\AbstractPiece;
-use Chess\Variant\Classical\PGN\AN\Color;
 use Chess\Variant\Classical\PGN\AN\Piece;
 
+/**
+ * Protection Evaluation
+ *
+ * If a piece is unprotected, it means that there are no other pieces defending
+ * it, and therefore it can be taken for free resulting in a material gain. This
+ * indicates a forcing move in that a player is to reply in a certain way. On
+ * the next turn, it should be defended by a piece or moved to a safe square.
+ * The player with the greater number of material points under protection has an
+ * advantage.
+ *
+ * @see \Chess\Eval\AttackEval
+ */
 class ProtectionEval extends AbstractEval implements
     ElaborateEvalInterface,
     ExplainEvalInterface
@@ -15,8 +26,16 @@ class ProtectionEval extends AbstractEval implements
     use ElaborateEvalTrait;
     use ExplainEvalTrait;
 
+    /**
+     * The name of the heuristic.
+     *
+     * @var string
+     */
     const NAME = 'Protection';
 
+    /**
+     * @param \Chess\Variant\AbstractBoard $board
+     */
     public function __construct(AbstractBoard $board)
     {
         $this->board = $board;
@@ -48,6 +67,11 @@ class ProtectionEval extends AbstractEval implements
         $this->explain($this->result);
     }
 
+    /**
+     * Elaborate on the evaluation.
+     *
+     * @param \Chess\Variant\AbstractPiece $piece
+     */
     private function elaborate(AbstractPiece $piece): void
     {
         $phrase = PiecePhrase::create($piece);
