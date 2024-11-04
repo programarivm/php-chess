f1ef865a11b715844c72d5cd398429f0dc57ce30
programarivm <info@programarivm.com>
2024-11-04 20:52:14 +0100

    Deleted the shorten() method

--- src/Eval/ElaborateEvalTrait.php
+++ src/Eval/ElaborateEvalTrait.php
@@ -13,32 +13,6 @@ trait ElaborateEvalTrait
         return $this->elaboration;
     }
 
-    protected function shorten(array $result, string $singular, string $plural): void
-    {
-        $sqs = [...$result[Color::W], ...$result[Color::B]];
-
-        if (count($sqs) > 1) {
-            $str = '';
-            $keys = array_keys($sqs);
-            $lastKey = end($keys);
-            foreach ($sqs as $key => $val) {
-                if ($key === $lastKey) {
-                    $str = substr($str, 0, -2);
-                    $str .= " and $val are {$plural}.";
-                } else {
-                    $str .= "$val, ";
-                }
-            }
-            $this->elaboration = [
-                $str,
-            ];
-        } elseif (count($sqs) === 1) {
-            $this->elaboration = [
-                "$sqs[0] is {$singular}.",
-            ];
-        }
-    }
-
     protected function reelaborate(string $intro, bool $ucfirst): array
     {
         if ($this->elaboration) {
