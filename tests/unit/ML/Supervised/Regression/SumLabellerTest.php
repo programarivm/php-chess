51b3dd0c8afca6dcab4338b788e6b0266f9a1e64
programarivm <info@programarivm.com>
2024-11-20 12:04:42 +0100

    Added SumLabellerTest

---
+++ tests/unit/ML/Supervised/Regression/SumLabellerTest.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Chess\Tests\Unit\ML\Supervised\Regression;
+
+use Chess\SanHeuristics;
+use Chess\Function\CompleteFunction;
+use Chess\ML\Supervised\Regression\SumLabeller;
+use Chess\Play\SanPlay;
+use Chess\Tests\AbstractUnitTestCase;
+
+class SumLabellerTest extends AbstractUnitTestCase
+{
+    static private CompleteFunction $function;
+
+    public static function setUpBeforeClass(): void
+    {
+        self::$function = new CompleteFunction();
+    }
+
+    /**
+     * @test
+     */
+    public function A00_labelled()
+    {
+        $name = 'Material';
+
+        $A00 = file_get_contents(self::DATA_FOLDER.'/sample/A00.pgn');
+
+        $board = (new SanPlay($A00))->validate()->board;
+
+        $balance = (new SanHeuristics(self::$function, $board->movetext(), $name))->getBalance();
+
+        $label = (new SumLabeller())->label($balance);
+
+        $expected = 0.0;
+
+        $this->assertSame($expected, $label);
+    }
+
+    /**
+     * @test
+     */
+    public function scholar_checkmate_labelled()
+    {
+        $name = 'Center';
+
+        $movetext = file_get_contents(self::DATA_FOLDER.'/sample/scholar_checkmate.pgn');
+
+        $board = (new SanPlay($movetext))->validate()->board;
+
+        $balance = (new SanHeuristics(self::$function, $board->movetext(), $name))->getBalance();
+
+        $label = (new SumLabeller())->label($balance);
+
+        $expected = 1.58;
+
+        $this->assertSame($expected, $label);
+    }
+
+    /**
+     * @test
+     */
+    public function A59_labelled()
+    {
+        $name = 'Connectivity';
+
+        $A59 = file_get_contents(self::DATA_FOLDER.'/sample/A59.pgn');
+
+        $board = (new SanPlay($A59))->validate()->board;
+
+        $balance = (new SanHeuristics(self::$function, $board->movetext(), $name))->getBalance();
+
+        $label = (new SumLabeller())->label($balance);
+
+        $expected = 2.0;
+
+        $this->assertSame($expected, $label);
+    }
+}
