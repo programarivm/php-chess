2b35d13f8d6d8869f40ed83f77fa6ec88433e8bf
programarivm <info@programarivm.com>
2024-12-02 17:58:08 +0100

    Optimized Chess\Eval\FlightSquareEval

--- src/Eval/FlightSquareEval.php
+++ src/Eval/FlightSquareEval.php
@@ -2,8 +2,6 @@
 
 namespace Chess\Eval;
 
-use Chess\Eval\SpaceEval;
-use Chess\Eval\SqCount;
 use Chess\Variant\AbstractBoard;
 use Chess\Variant\Classical\PGN\AN\Piece;
 
@@ -42,14 +40,11 @@ class FlightSquareEval extends AbstractEval implements
             "has more safe squares to move to than its counterpart",
         ];
 
-        $spEval = (new SpaceEval($this->board))->getResult();
-        $sqCount = (new SqCount($this->board))->count();
-
         foreach ($this->board->pieces() as $piece) {
             if ($piece->id === Piece::K) {
                 foreach ($piece->mobility as $sq) {
-                    if (!in_array($sq, $spEval[$piece->oppColor()]) &&
-                        in_array($sq, $sqCount['free'])
+                    if (!in_array($sq, $this->board->spaceEval[$piece->oppColor()]) &&
+                        in_array($sq, $this->board->sqCount['free'])
                     ) {
                         $this->result[$piece->color] += 1;
                     }
