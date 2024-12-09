e96a26db77c3e33fd85358a08f4418a5a3fca11a
programarivm <info@programarivm.com>
2024-12-09 21:17:52 +0100

    Updated Chess\FenHeuristics

--- src/FenHeuristics.php
+++ src/FenHeuristics.php
@@ -22,46 +22,32 @@ class FenHeuristics
         $this->function = $function;
         $this->board = $board;
 
-        $this->calc()->ternarize();
-    }
-
-    protected function calc(): FenHeuristics
-    {
         foreach ($this->function->getEval() as $val) {
             $eval = new $val($this->board);
-            $result = $eval->getResult();
-            if (is_array($result[Color::W])) {
+            $item = $eval->getResult();
+            if (is_array($item[Color::W])) {
                 if ($eval instanceof InverseEvalInterface) {
-                    $this->result[] = [
-                        Color::W => count($result[Color::B]),
-                        Color::B => count($result[Color::W]),
+                    $item = [
+                        Color::W => count($item[Color::B]),
+                        Color::B => count($item[Color::W]),
                     ];
                 } else {
-                    $this->result[] = [
-                        Color::W => count($result[Color::W]),
-                        Color::B => count($result[Color::B]),
+                    $item = [
+                        Color::W => count($item[Color::W]),
+                        Color::B => count($item[Color::B]),
                     ];
                 }
             } else {
                 if ($eval instanceof InverseEvalInterface) {
-                    $this->result[] = [
-                        Color::W => $result[Color::B],
-                        Color::B => $result[Color::W],
+                    $item = [
+                        Color::W => $item[Color::B],
+                        Color::B => $item[Color::W],
                     ];
-                } else {
-                    $this->result[] = $result;
                 }
             }
-        }
-
-        return $this;
-    }
-
-    protected function ternarize(): void
-    {
-        foreach ($this->result as $key => $val) {
-            $diff = $val[Color::W] - $val[Color::B];
-            $this->balance[$key] = $diff > 0 ? 1 : ($diff < 0 ? -1 : 0);
+            $this->result[] = $item;
+            $diff = $item[Color::W] - $item[Color::B];
+            $this->balance[] = $diff > 0 ? 1 : ($diff < 0 ? -1 : 0);
         }
     }
 }
