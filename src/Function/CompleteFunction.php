0277c5786dbdf12df36e0ea131cacc86e7e21864
programarivm <info@programarivm.com>
2024-12-11 10:18:48 +0100

    Updated Chess\Function\CompleteFunction

--- src/Function/CompleteFunction.php
+++ src/Function/CompleteFunction.php
@@ -2,74 +2,17 @@
 
 namespace Chess\Function;
 
-use Chess\Eval\AbsoluteForkEval;
-use Chess\Eval\AbsolutePinEval;
-use Chess\Eval\AbsoluteSkewerEval;
-use Chess\Eval\AdvancedPawnEval;
 use Chess\Eval\AttackEval;
-use Chess\Eval\BackRankThreatEval;
-use Chess\Eval\BackwardPawnEval;
-use Chess\Eval\BadBishopEval;
-use Chess\Eval\BishopOutpostEval;
-use Chess\Eval\BishopPairEval;
-use Chess\Eval\CenterEval;
 use Chess\Eval\CheckabilityEval;
-use Chess\Eval\ConnectivityEval;
-use Chess\Eval\DefenseEval;
-use Chess\Eval\DiagonalOppositionEval;
-use Chess\Eval\DirectOppositionEval;
-use Chess\Eval\DiscoveredCheckEval;
-use Chess\Eval\DoubledPawnEval;
-use Chess\Eval\FarAdvancedPawnEval;
-use Chess\Eval\FlightSquareEval;
-use Chess\Eval\InverseEvalInterface;
-use Chess\Eval\IsolatedPawnEval;
-use Chess\Eval\KingSafetyEval;
-use Chess\Eval\KnightOutpostEval;
-use Chess\Eval\MaterialEval;
-use Chess\Eval\OverloadingEval;
-use Chess\Eval\PassedPawnEval;
-use Chess\Eval\PressureEval;
-use Chess\Eval\ProtectionEval;
-use Chess\Eval\RelativeForkEval;
-use Chess\Eval\RelativePinEval;
-use Chess\Eval\SpaceEval;
-use Chess\Eval\SqOutpostEval;
 
-class CompleteFunction extends AbstractFunction
+class CompleteFunction extends FastFunction
 {
-    protected array $eval = [
-        MaterialEval::class,
-        CenterEval::class,
-        ConnectivityEval::class,
-        SpaceEval::class,
-        PressureEval::class,
-        KingSafetyEval::class,
-        ProtectionEval::class,
-        DiscoveredCheckEval::class,
-        DoubledPawnEval::class,
-        PassedPawnEval::class,
-        AdvancedPawnEval::class,
-        FarAdvancedPawnEval::class,
-        IsolatedPawnEval::class,
-        BackwardPawnEval::class,
-        DefenseEval::class,
-        AbsoluteSkewerEval::class,
-        AbsolutePinEval::class,
-        RelativePinEval::class,
-        AbsoluteForkEval::class,
-        RelativeForkEval::class,
-        SqOutpostEval::class,
-        KnightOutpostEval::class,
-        BishopOutpostEval::class,
-        BishopPairEval::class,
-        BadBishopEval::class,
-        DiagonalOppositionEval::class,
-        DirectOppositionEval::class,
-        AttackEval::class,
-        OverloadingEval::class,
-        BackRankThreatEval::class,
-        CheckabilityEval::class,
-        FlightSquareEval::class,
-    ];
+    public function __construct()
+    {
+        $this->eval = [
+            ...$this->eval,
+            AttackEval::class,
+            CheckabilityEval::class,
+        ];
+    }
 }
