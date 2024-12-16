251c033cb6fcbc602b6b6e4c87d514852e7d8317
programarivm <info@programarivm.com>
2024-12-16 18:13:19 +0100

    Updated Chess\Glossary\ElaborateTermTrait

--- src/Glossary/ElaborateTermTrait.php
+++ src/Glossary/ElaborateTermTrait.php
@@ -5,27 +5,6 @@ namespace Chess\Glossary;
 trait ElaborateTermTrait
 {
     public array $toElaborate = [];
-    
+
     public array $elaboration = [];
-
-    protected function shorten(string $intro, bool $ucfirst): array
-    {
-        if ($this->elaboration) {
-            $rephrase = '';
-
-            foreach ($this->elaboration as $val) {
-                $rephrase .= $val . ', ';
-            }
-
-            if ($ucfirst) {
-                $rephrase = ucfirst($rephrase);
-            }
-
-            $this->elaboration = [
-                $intro . substr_replace($rephrase, '.', -2),
-            ];
-        }
-
-        return $this->elaboration;
-    }
 }
