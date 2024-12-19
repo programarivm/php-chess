2e0173301f007c89a824f7c1f18a5548b5f9169f
programarivm <info@programarivm.com>
2024-12-19 09:59:18 +0100

    Added Chess\ML\PrimeLabeller

---
+++ src/ML/PrimeLabeller.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Chess\ML;
+
+class PrimeLabeller extends AbstractLabeller
+{
+    public function label(array $end)
+    {
+        // TODO 
+
+        return 1;
+    }
+}
---
+++ tests/unit/ML/PrimeLabellerTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Chess\Tests\Unit\ML;
+
+use Chess\FenHeuristics;
+use Chess\Function\CompleteFunction;
+use Chess\ML\PrimeLabeller;
+use Chess\Play\SanPlay;
+use Chess\Tests\AbstractUnitTestCase;
+
+class PrimeLabellerTest extends AbstractUnitTestCase
+{
+    static private CompleteFunction $function;
+
+    public static function setUpBeforeClass(): void
+    {
+        self::$function = new CompleteFunction();
+    }
+
+    /**
+     * @test
+     */
+    public function A00()
+    {
+        $A00 = file_get_contents(self::DATA_FOLDER.'/sample/A00.pgn');
+
+        $board = (new SanPlay($A00))->validate()->board;
+
+        $balance = (new FenHeuristics(self::$function, $board))->balance;
+
+        $label = (new PrimeLabeller())->label($balance);
+
+        $expected = 0;
+
+        $this->assertSame($expected, $label);
+    }
+}
