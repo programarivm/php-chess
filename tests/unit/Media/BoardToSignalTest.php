9924597e959a029a329e92a1415cefd1dd324ed2
Jordi Bassagana <info@programarivm.com>
2024-12-19 12:20:19 +0100

    Implemented Chess\Media\BoardToSignal (#694)

---
+++ tests/unit/Media/BoardToSignalTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Chess\Tests\Unit\Media;
+
+use Chess\Media\BoardToSignal;
+use Chess\Tests\AbstractUnitTestCase;
+use Chess\Variant\Classical\Board;
+
+class BoardToSignalTest extends AbstractUnitTestCase
+{
+    /**
+     * @test
+     */
+    public function A74()
+    {
+        $expected = [
+            6984,
+            1392,
+            6978,
+            132714,
+            258432,
+            -48,
+            1944,
+            53624864,
+            2022,
+            7044,
+            95632,
+            95632,
+            95632,
+            93994,
+            84088,
+            12146824,
+            95632,
+            95632,
+            94618,
+        ];
+
+        $A74 = file_get_contents(self::DATA_FOLDER.'/sample/A74.pgn');
+
+        $board = new Board();
+
+        $signal = (new BoardToSignal($A74, $board))->signal;
+
+        $this->assertSame($expected, $signal);
+    }
+}
