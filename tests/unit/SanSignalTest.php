e01c23b4f40d9efad819d2f5e39b3c22e6367188
programarivm <info@programarivm.com>
2024-12-22 15:58:12 +0100

    Updated Chess\Eval\CenterEval

--- tests/unit/SanSignalTest.php
+++ tests/unit/SanSignalTest.php
@@ -23,11 +23,11 @@ class SanSignalTest extends AbstractUnitTestCase
      */
     public function e4_d5_exd5_Qxd5()
     {
-        $expectedCenter = [ 0.0, 1.0, 0.08, 0.67, -1.0 ];
+        $expectedCenter = [ 0, 1.0, 0.09, 0.65, -1.0 ];
         $expectedConnectivity = [ 0.0, -1.0, -1.0, -1.0, 1.0 ];
         $expectedSpace = [ 0.0, 1.0, 0.25, 0.50, -1.0 ];
-        $expectedUnnormalized = [ 0.0, 20.0, 1.0, 10.0, -63.5 ];
-        $expectedNormalized = [ 0.0, 2.0, -1.67, -0.16, -5.0 ];
+        $expectedUnnormalized = [ 0.0, 20.3, 1.1, 10.0, -64.0 ];
+        $expectedNormalized = [ 0.0, 2.0, -1.66, -0.18, -5.0 ];
 
         $movetext = '1.e4 d5 2.exd5 Qxd5';
         $sanSignal = new SanSignal(self::$function, $movetext, new Board());
@@ -44,8 +44,8 @@ class SanSignalTest extends AbstractUnitTestCase
      */
     public function A59()
     {
-        $expectedUnnormalized = [ 0.0, 18.0, 2.6, 11.6, 4.6, 5.6, 1.6, 6.6, 4.6, 2.6, -7.4, 3.0, -2.0, 7.0, -4.1, 12.9, 8.9, 9.9 ];
-        $expectedNormalized = [ 0.0, 2.5, 0.77, 1.01, 1.31, 1.4, 1.69, 3.52, 3.03, 2.77, 0.0, 1.85, 0.17, 0.88, -2.13, 3.42, 2.07, 2.24 ];
+        $expectedUnnormalized = [ 0.0, 18.3, 2.48, 11.48, 4.48, 5.48, 1.48, 6.48, 4.48, 2.48, -7.52, 2.98, -2.12, 7.08, -4.02, 13.08, 8.98, 9.98 ];
+        $expectedNormalized = [ 0.0, 2.5, 0.75, 0.98, 1.3, 1.38, 1.67, 3.5, 3.01, 2.75, 0.0, 1.85, 0.16, 0.87, -2.13, 3.41, 2.05, 2.22 ];
 
         $A59 = file_get_contents(self::DATA_FOLDER.'/sample/A59.pgn');
         $sanSignal = new SanSignal(self::$function, $A59, new Board());
@@ -119,7 +119,7 @@ class SanSignalTest extends AbstractUnitTestCase
      */
     public function d4()
     {
-        $expectedUnnormalized = [ 0.0, 18.0 ];
+        $expectedUnnormalized = [ 0.0, 18.3 ];
         $expectedNormalized = [ 0.0, 3.0 ];
 
         $movetext = '1.d4';
@@ -134,7 +134,7 @@ class SanSignalTest extends AbstractUnitTestCase
      */
     public function e4()
     {
-        $expectedUnnormalized = [ 0.0, 20.0 ];
+        $expectedUnnormalized = [ 0.0, 20.3 ];
         $expectedNormalized = [ 0.0, 2.0 ];
 
         $movetext = '1.e4';
@@ -149,7 +149,7 @@ class SanSignalTest extends AbstractUnitTestCase
      */
     public function f4()
     {
-        $expectedUnnormalized = [ 0.0, 9.0 ];
+        $expectedUnnormalized = [ 0.0, 9.3 ];
         $expectedNormalized = [ 0.0, 2.0 ];
 
         $movetext = '1.f4';
@@ -164,7 +164,7 @@ class SanSignalTest extends AbstractUnitTestCase
      */
     public function g4()
     {
-        $expectedUnnormalized = [ 0.0, 5.0 ];
+        $expectedUnnormalized = [ 0.0, 5.2 ];
         $expectedNormalized = [ 0.0, 1.0 ];
 
         $movetext = '1.g4';
@@ -179,7 +179,7 @@ class SanSignalTest extends AbstractUnitTestCase
      */
     public function h4()
     {
-        $expectedUnnormalized = [ 0.0, 3.0 ];
+        $expectedUnnormalized = [ 0.0, 3.1 ];
         $expectedNormalized = [ 0.0, 2.0 ];
 
         $movetext = '1.h4';
@@ -194,8 +194,8 @@ class SanSignalTest extends AbstractUnitTestCase
      */
     public function a4_h5()
     {
-        $expectedUnnormalized = [ 0.0, 3.0, 0.0 ];
-        $expectedNormalized = [ 0.0, 2.0, 0.0 ];
+        $expectedUnnormalized = [ 0.0, 3.0, -0.1 ];
+        $expectedNormalized = [ 0.0, 2.0, -1.0 ];
 
         $movetext = '1.a4 h5';
         $sanSignal = new SanSignal(self::$function, $movetext, new Board());
@@ -224,7 +224,7 @@ class SanSignalTest extends AbstractUnitTestCase
      */
     public function a4_e5()
     {
-        $expectedUnnormalized = [ 0.0, 3.0, -17.0 ];
+        $expectedUnnormalized = [ 0.0, 3.0, -17.3 ];
         $expectedNormalized = [ 0.0, 2.0, -2.0 ];
 
         $movetext = '1.a4 e5';
@@ -239,7 +239,7 @@ class SanSignalTest extends AbstractUnitTestCase
      */
     public function h4_e5()
     {
-        $expectedUnnormalized = [ 0.0, 3.0, -17.0 ];
+        $expectedUnnormalized = [ 0.0, 3.1, -17.2 ];
         $expectedNormalized = [ 0.0, 2.0, -3.0 ];
 
         $movetext = '1.h4 e5';
