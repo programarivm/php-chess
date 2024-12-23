ce7aadc202b6f1e2e15618e4eba32d6123889a85
programarivm <info@programarivm.com>
2024-12-23 10:35:55 +0100

    Updated tests

--- tests/unit/SanSignalTest.php
+++ tests/unit/SanSignalTest.php
@@ -45,11 +45,13 @@ class SanSignalTest extends AbstractUnitTestCase
     public function A59()
     {
         $expectedTime = [ 0.0, 2.5, 0.78, 1.0, 1.32, 1.4, 1.7, 3.53, 3.04, 2.78, 0.0, 1.86, 0.19, 0.89, -2.17, 3.41, 2.05, 2.23 ];
+        $expectedFreq = [ 0.0, 1.62, 0.5, 1.12, 2.35, 1.23, 1.7, 2.2, 0.7, 3.67, 3.88, 1.2, -0.95, -0.55, -0.72, 0.3, 0.29, 0.3 ];
 
         $A59 = file_get_contents(self::DATA_FOLDER.'/sample/A59.pgn');
         $sanSignal = new SanSignal(self::$function, $A59, new Board());
 
         $this->assertEquals($expectedTime, $sanSignal->time);
+        $this->assertEquals($expectedFreq, $sanSignal->freq);
     }
 
     /**
@@ -58,11 +60,13 @@ class SanSignalTest extends AbstractUnitTestCase
     public function a3()
     {
         $expectedTime = [ 0.0, 2.0 ];
+        $expectedFreq = [ 0.0, 2.0 ];
 
         $movetext = '1.a3';
         $sanSignal = new SanSignal(self::$function, $movetext, new Board());
 
         $this->assertEquals($expectedTime, $sanSignal->time);
+        $this->assertEquals($expectedFreq, $sanSignal->freq);
     }
 
     /**
@@ -71,11 +75,13 @@ class SanSignalTest extends AbstractUnitTestCase
     public function a4()
     {
         $expectedTime = [ 0.0, 2.0 ];
+        $expectedFreq = [ 0.0, 1.5 ];
 
         $movetext = '1.a4';
         $sanSignal = new SanSignal(self::$function, $movetext, new Board());
 
         $this->assertEquals($expectedTime, $sanSignal->time);
+        $this->assertEquals($expectedFreq, $sanSignal->freq);
     }
 
     /**
@@ -84,11 +90,13 @@ class SanSignalTest extends AbstractUnitTestCase
     public function b4()
     {
         $expectedTime = [ 0.0, 1.0 ];
+        $expectedFreq = [ 0.0, 1.0 ];
 
         $movetext = '1.b4';
         $sanSignal = new SanSignal(self::$function, $movetext, new Board());
 
         $this->assertEquals($expectedTime, $sanSignal->time);
+        $this->assertEquals($expectedFreq, $sanSignal->freq);
     }
 
     /**
@@ -97,11 +105,13 @@ class SanSignalTest extends AbstractUnitTestCase
     public function c4()
     {
         $expectedTime = [ 0.0, 1.0 ];
+        $expectedFreq = [ 0.0, 0.67 ];
 
         $movetext = '1.c4';
         $sanSignal = new SanSignal(self::$function, $movetext, new Board());
 
         $this->assertEquals($expectedTime, $sanSignal->time);
+        $this->assertEquals($expectedFreq, $sanSignal->freq);
     }
 
     /**
@@ -110,11 +120,13 @@ class SanSignalTest extends AbstractUnitTestCase
     public function d4()
     {
         $expectedTime = [ 0.0, 3.0 ];
+        $expectedFreq = [ 0.0, 1.62 ];
 
         $movetext = '1.d4';
         $sanSignal = new SanSignal(self::$function, $movetext, new Board());
 
         $this->assertEquals($expectedTime, $sanSignal->time);
+        $this->assertEquals($expectedFreq, $sanSignal->freq);
     }
 
     /**
@@ -123,11 +135,13 @@ class SanSignalTest extends AbstractUnitTestCase
     public function e4()
     {
         $expectedTime = [ 0.0, 2.0 ];
+        $expectedFreq = [ 0.0, 0.73 ];
 
         $movetext = '1.e4';
         $sanSignal = new SanSignal(self::$function, $movetext, new Board());
 
         $this->assertEquals($expectedTime, $sanSignal->time);
+        $this->assertEquals($expectedFreq, $sanSignal->freq);
     }
 
     /**
@@ -136,11 +150,13 @@ class SanSignalTest extends AbstractUnitTestCase
     public function f4()
     {
         $expectedTime = [ 0.0, 2.0 ];
+        $expectedFreq = [ 0.0, 0.64 ];
 
         $movetext = '1.f4';
         $sanSignal = new SanSignal(self::$function, $movetext, new Board());
 
         $this->assertEquals($expectedTime, $sanSignal->time);
+        $this->assertEquals($expectedFreq, $sanSignal->freq);
     }
 
     /**
@@ -149,11 +165,13 @@ class SanSignalTest extends AbstractUnitTestCase
     public function g4()
     {
         $expectedTime = [ 0.0, 1.0 ];
+        $expectedFreq = [ 0.0, 0.91 ];
 
         $movetext = '1.g4';
         $sanSignal = new SanSignal(self::$function, $movetext, new Board());
 
         $this->assertEquals($expectedTime, $sanSignal->time);
+        $this->assertEquals($expectedFreq, $sanSignal->freq);
     }
 
     /**
@@ -162,11 +180,13 @@ class SanSignalTest extends AbstractUnitTestCase
     public function h4()
     {
         $expectedTime = [ 0.0, 2.0 ];
+        $expectedFreq = [ 0.0, 1.6 ];
 
         $movetext = '1.h4';
         $sanSignal = new SanSignal(self::$function, $movetext, new Board());
 
         $this->assertEquals($expectedTime, $sanSignal->time);
+        $this->assertEquals($expectedFreq, $sanSignal->freq);
     }
 
     /**
@@ -175,11 +195,13 @@ class SanSignalTest extends AbstractUnitTestCase
     public function a4_h5()
     {
         $expectedTime = [ 0.0, 2.0, -1.0 ];
+        $expectedFreq = [ 0.0, 1.5, -1.0 ];
 
         $movetext = '1.a4 h5';
         $sanSignal = new SanSignal(self::$function, $movetext, new Board());
 
         $this->assertEquals($expectedTime, $sanSignal->time);
+        $this->assertEquals($expectedFreq, $sanSignal->freq);
     }
 
     /**
@@ -188,11 +210,13 @@ class SanSignalTest extends AbstractUnitTestCase
     public function a4_a5()
     {
         $expectedTime = [ 0.0, 2.0, 0.0 ];
+        $expectedFreq = [ 0.0, 1.5, 0.0 ];
 
         $movetext = '1.a4 a5';
         $sanSignal = new SanSignal(self::$function, $movetext, new Board());
 
         $this->assertEquals($expectedTime, $sanSignal->time);
+        $this->assertEquals($expectedFreq, $sanSignal->freq);
     }
 
     /**
@@ -201,11 +225,13 @@ class SanSignalTest extends AbstractUnitTestCase
     public function a4_e5()
     {
         $expectedTime = [ 0.0, 2.0, -2.0 ];
+        $expectedFreq = [ 0.0, 1.5, -0.62 ];
 
         $movetext = '1.a4 e5';
         $sanSignal = new SanSignal(self::$function, $movetext, new Board());
 
         $this->assertEquals($expectedTime, $sanSignal->time);
+        $this->assertEquals($expectedFreq, $sanSignal->freq);
     }
 
     /**
@@ -214,11 +240,13 @@ class SanSignalTest extends AbstractUnitTestCase
     public function h4_e5()
     {
         $expectedTime = [ 0.0, 2.0, -3.0 ];
+        $expectedFreq = [ 0.0, 1.6, -0.63 ];
 
         $movetext = '1.h4 e5';
         $sanSignal = new SanSignal(self::$function, $movetext, new Board());
 
         $this->assertEquals($expectedTime, $sanSignal->time);
+        $this->assertEquals($expectedFreq, $sanSignal->freq);
     }
 
     /**
@@ -227,6 +255,7 @@ class SanSignalTest extends AbstractUnitTestCase
     public function a5()
     {
         $expectedTime = [ 0.0, 4.0 ];
+        $expectedFreq = [ 0.0, 4.0 ];
 
         $fen = '7k/8/8/8/P7/8/8/7K w - -';
         $movetext = '1.a5';
@@ -234,6 +263,7 @@ class SanSignalTest extends AbstractUnitTestCase
         $sanSignal = new SanSignal(self::$function, $movetext, $board);
 
         $this->assertEquals($expectedTime, $sanSignal->time);
+        $this->assertEquals($expectedFreq, $sanSignal->freq);
     }
 
     /**
@@ -242,6 +272,7 @@ class SanSignalTest extends AbstractUnitTestCase
     public function a6()
     {
         $expectedTime = [ 0.0, 5.0 ];
+        $expectedFreq = [ 0.0, 5.0 ];
 
         $fen = '7k/8/8/P7/8/8/8/7K w - -';
         $movetext = '1.a6';
@@ -249,6 +280,7 @@ class SanSignalTest extends AbstractUnitTestCase
         $sanSignal = new SanSignal(self::$function, $movetext, $board);
 
         $this->assertEquals($expectedTime, $sanSignal->time);
+        $this->assertEquals($expectedFreq, $sanSignal->freq);
     }
 
     /**
@@ -257,6 +289,7 @@ class SanSignalTest extends AbstractUnitTestCase
     public function a7()
     {
         $expectedTime = [ 0.0, 3.0 ];
+        $expectedFreq = [ 0.0, 3.0 ];
 
         $fen = '4k3/8/P7/8/8/8/8/4K3 w - -';
         $movetext = '1.a7';
@@ -264,6 +297,7 @@ class SanSignalTest extends AbstractUnitTestCase
         $sanSignal = new SanSignal(self::$function, $movetext, $board);
 
         $this->assertEquals($expectedTime, $sanSignal->time);
+        $this->assertEquals($expectedFreq, $sanSignal->freq);
     }
 
     /**
@@ -272,6 +306,7 @@ class SanSignalTest extends AbstractUnitTestCase
     public function b5()
     {
         $expectedTime = [ 0.0, 4.0 ];
+        $expectedFreq = [ 0.0, 1.33 ];
 
         $fen = '7k/8/8/8/1P6/8/8/7K w - -';
         $movetext = '1.b5';
@@ -279,6 +314,7 @@ class SanSignalTest extends AbstractUnitTestCase
         $sanSignal = new SanSignal(self::$function, $movetext, $board);
 
         $this->assertEquals($expectedTime, $sanSignal->time);
+        $this->assertEquals($expectedFreq, $sanSignal->freq);
     }
 
     /**
@@ -287,6 +323,7 @@ class SanSignalTest extends AbstractUnitTestCase
     public function h7()
     {
         $expectedTime = [ 0.0, 4.0 ];
+        $expectedFreq = [ 0.0, 3.2 ];
 
         $fen = '4k3/8/7P/8/8/8/8/4K3 w - -';
         $movetext = '1.h7';
@@ -294,5 +331,6 @@ class SanSignalTest extends AbstractUnitTestCase
         $sanSignal = new SanSignal(self::$function, $movetext, $board);
 
         $this->assertEquals($expectedTime, $sanSignal->time);
+        $this->assertEquals($expectedFreq, $sanSignal->freq);
     }
 }
