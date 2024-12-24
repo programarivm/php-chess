1081b14fbcf6fe4ced424480ab4d41c292015657
programarivm <info@programarivm.com>
2024-12-24 12:31:26 +0100

    Updated Chess\FenHeuristics

--- src/FenHeuristics.php
+++ src/FenHeuristics.php
@@ -2,7 +2,6 @@
 
 namespace Chess;
 
-use Chess\Eval\InverseEvalInterface;
 use Chess\Function\AbstractFunction;
 use Chess\Variant\AbstractBoard;
 use Chess\Variant\Classical\PGN\AN\Color;
@@ -35,28 +34,7 @@ class FenHeuristics
     public function __construct(AbstractFunction $f, AbstractBoard $board)
     {
         foreach ($f->eval as $val) {
-            $eval = new $val($board);
-            $item = $eval->result;
-            if (is_array($item[Color::W])) {
-                if ($eval instanceof InverseEvalInterface) {
-                    $item = [
-                        Color::W => count($item[Color::B]),
-                        Color::B => count($item[Color::W]),
-                    ];
-                } else {
-                    $item = [
-                        Color::W => count($item[Color::W]),
-                        Color::B => count($item[Color::B]),
-                    ];
-                }
-            } else {
-                if ($eval instanceof InverseEvalInterface) {
-                    $item = [
-                        Color::W => $item[Color::B],
-                        Color::B => $item[Color::W],
-                    ];
-                }
-            }
+            $item = EvalArray::add(new $val($board));
             $this->result[] = $item;
             $diff = $item[Color::W] - $item[Color::B];
             $this->balance[] = $diff > 0 ? 1 : ($diff < 0 ? -1 : 0);
