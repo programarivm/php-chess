e3bfe2397346cfd021c3fc0a98fd1b2be22f2111
programarivm <info@programarivm.com>
2025-01-05 22:19:29 +0100

    Renamed UniqueEvalInterface as UniqueImbalanceEvalInterface

---
+++ src/Eval/UniqueImbalanceEvalInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Chess\Eval;
+
+/**
+ * Unique Imbalance Evaluation
+ *
+ * Classes implementing this interface are designed so that each move can create
+ * a unique imbalance.
+ */
+interface UniqueImbalanceEvalInterface
+{
+}
