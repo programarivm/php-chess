6b5c3e94a6fcbf2f6aba426ff9d8ba4fec912d5a
programarivm <info@programarivm.com>
2025-01-06 13:12:36 +0100

    Improved the docs

--- src/Eval/UniqueImbalanceEvalInterface.php
+++ src/Eval/UniqueImbalanceEvalInterface.php
@@ -6,7 +6,16 @@ namespace Chess\Eval;
  * Unique Imbalance Evaluation
  *
  * Classes implementing this interface are designed so that each move can create
- * a unique imbalance.
+ * a unique sufficiently small imbalance. It can be seen as a correction factor
+ * to break the symmetry of the system, it is used to adjust the evaluation as
+ * per Blab's theorem:
+ *
+ * "A chess position can be obtained from the last oscillation of a chessboard."
+ *
+ * FEN(n) ≡ Ψ(n)
+ *
+ * Blab's corollary says that an entire chess game can be obtained from a signal
+ * encoding the oscillations of a chessboard.
  */
 interface UniqueImbalanceEvalInterface
 {
