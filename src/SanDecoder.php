4fe07e19b99dfe4aa8314644355497c2e214a282
programarivm <info@programarivm.com>
2025-01-06 20:52:38 +0100

    Documented Chess\SanDecoder

--- src/SanDecoder.php
+++ src/SanDecoder.php
@@ -6,8 +6,21 @@ use Chess\EvalArray;
 use Chess\Function\AbstractFunction;
 use Chess\Variant\AbstractBoard;
 
+/**
+ * SAN Decoder
+ *
+ * Decodes the oscillations of all evaluation features into a chess board.
+ */
 class SanDecoder
 {
+    /**
+     * Returns a chess board.
+     *
+     * @param \Chess\Function\AbstractFunction $f
+     * @param \Chess\Variant\AbstractBoard $board
+     * @param array $mean
+     * @return \Chess\Variant\AbstractBoard
+     */
     public static function mean(AbstractFunction $f, AbstractBoard $board, array $mean): AbstractBoard
     {
         for ($i = 1; $i < count($mean); $i++) {
