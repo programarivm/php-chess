2f22ac7a83979b4164bd78eb570129b9cb02ed78
standard <info@programarivm.com>
2025-01-15 12:59:00 +0100

    Replaced EvalArray with FunctionTrait

--- src/Function/FunctionTrait.php
+++ src/Function/FunctionTrait.php
@@ -2,7 +2,11 @@
 
 namespace Chess\Function;
 
+use Chess\Eval\AbstractEval;
+use Chess\Eval\InverseEvalInterface;
+use Chess\Function\AbstractFunction;
 use Chess\Variant\AbstractBoard;
+use Chess\Variant\Classical\PGN\Color;
 
 trait FunctionTrait
 {
@@ -42,5 +46,177 @@ trait FunctionTrait
         }
 
         return self::normalize(-1, 1, $items);
+    }
+
+    /**
+     * A strong position can be created by accumulating small advantages. The
+     * relative value of the position without considering checkmate is obtained
+     * by counting the advantages in the evaluation array.
+     *
+     * @param \Chess\Variant\AbstractBoard $board
+     * @return int
+     */
+    public static function steinitz(AbstractBoard $board): int
+    {
+        $count = 0;
+        $normd = array_filter(self::normalization($board));
+        foreach ($normd as $val) {
+            if ($val > 0) {
+                $count += 1;
+            } elseif ($val < 0) {
+                $count -= 1;
+            }
+        }
+
+        return $count;
+    }
+
+    /**
+     * Returns the mean of the elements in the array.
+     *
+     * @param \Chess\Variant\AbstractBoard $board
+     * @return float
+     */
+    public static function mean(AbstractBoard $board): float
+    {
+        $normd = array_filter(self::normalization($board));
+        $sum = array_sum($normd);
+        $count = count($normd);
+        if ($count > 0) {
+            return round($sum / $count, 4);
+        }
+
+        return 0.0;
+    }
+
+    /**
+     * Returns the value in the middle of the array.
+     *
+     * @param \Chess\Variant\AbstractBoard $board
+     * @return float
+     */
+    public static function median(AbstractBoard $board): float
+    {
+        $normd = array_filter(self::normalization($board));
+        sort($normd);
+        $size = sizeof($normd);
+        if ($size % 2 == 0) {
+            return ($normd[$size / 2] + $normd[$size / 2 - 1]) / 2;
+        }
+
+        return $normd[floor($size / 2)];
+    }
+
+    /**
+     * Returns the most common number in the array.
+     *
+     * @param \Chess\Variant\AbstractBoard $board
+     * @return null|float
+     */
+    public static function mode(AbstractBoard $board): ?float
+    {
+        $normd = array_filter(self::normalization($board));
+        foreach ($normd as &$val) {
+            $val = strval($val);
+        }
+        $values = array_count_values($normd);
+        arsort($values);
+        if (current($values) > 1) {
+            return floatval(array_key_first($values));
+        }
+
+        return null;
+    }
+
+    /**
+     * Returns a measure of how spread out the array is.
+     *
+     * @param \Chess\Variant\AbstractBoard $board
+     * @return float
+     */
+    public static function var(AbstractBoard $board): float
+    {
+        $normd = array_filter(self::normalization($board));
+        $mean = self::mean($board);
+        $sum = 0;
+        foreach ($normd as $val) {
+            $diff = $val - $mean;
+            $sum += $diff * $diff;
+        }
+
+        return round($sum / count($normd), 4);
+    }
+
+    /**
+     * Returns a measure of how spread out the array is.
+     *
+     * @param \Chess\Variant\AbstractBoard $board
+     * @return float
+     */
+    public static function sd(AbstractBoard $board): float
+    {
+        $var = self::var($board);
+
+        return round(sqrt($var), 4);
+    }
+
+    /**
+     * Add an item to the array.
+     *
+     * @param \Chess\Eval\AbstractEval $eval
+     * @return array
+     */
+    public static function add(AbstractEval $eval): array
+    {
+        if (is_array($eval->result[Color::W])) {
+            if ($eval instanceof InverseEvalInterface) {
+                $item = [
+                    Color::W => count($eval->result[Color::B]),
+                    Color::B => count($eval->result[Color::W]),
+                ];
+            } else {
+                $item = [
+                    Color::W => count($eval->result[Color::W]),
+                    Color::B => count($eval->result[Color::B]),
+                ];
+            }
+        } else {
+            if ($eval instanceof InverseEvalInterface) {
+                $item = [
+                    Color::W => $eval->result[Color::B],
+                    Color::B => $eval->result[Color::W],
+                ];
+            } else {
+                $item = $eval->result;
+            }
+        }
+
+        return $item;
+    }
+
+    /**
+     * Normalizes the given array.
+     *
+     * @param int $newMin
+     * @param int $newMax
+     * @param array $unnormd
+     * @return array
+     */
+    public static function normalize(int $newMin, int $newMax, array $unnormd): array
+    {
+        $min = min($unnormd);
+        $max = max($unnormd);
+
+        foreach ($unnormd as $key => $val) {
+            if ($val > 0) {
+                $unnormd[$key] = round($unnormd[$key] * $newMax / $max, 4);
+            } elseif ($val < 0) {
+                $unnormd[$key] = round($unnormd[$key] * $newMin / $min, 4);
+            } else {
+                $unnormd[$key] = 0;
+            }
+        }
+
+        return $unnormd;
     }
 }
--- tests/unit/Function/FastFunctionTest.php
+++ tests/unit/Function/FastFunctionTest.php
@@ -2,6 +2,7 @@
 
 namespace Chess\Tests\Unit\Function;
 
+use Chess\FenToBoardFactory;
 use Chess\Function\FastFunction;
 use Chess\Tests\AbstractUnitTestCase;
 
@@ -46,5 +47,39 @@ class FastFunctionTest extends AbstractUnitTestCase
         ];
 
         $this->assertSame($expected, FastFunction::names());
+    }
+
+    /**
+     * @test
+     */
+    public function kaufman_06()
+    {
+        $expectedNormd = [ -1.0, -0.3333, 0.0112, 0.0865, 0.0865, 0.0865, 0.4325, 1.0 ];
+        $expectedSteinitz = 4;
+        $expectedMean = 0.0462;
+        $expectedMedian = 0.0865;
+        $expectedMode = 0.0865;
+        $expectedVar = 0.288;
+        $expectedSd = 0.5367;
+
+        $board = FenToBoardFactory::create('r5k1/3n1ppp/1p6/3p1p2/3P1B2/r3P2P/PR3PP1/2R3K1 b - -');
+
+        $normd = array_filter(FastFunction::normalization($board));
+        sort($normd);
+
+        $steinitz = FastFunction::steinitz($board);
+        $mean = FastFunction::mean($board);
+        $median = FastFunction::median($board);
+        $mode = FastFunction::mode($board);
+        $var = FastFunction::var($board);
+        $sd = FastFunction::sd($board);
+
+        $this->assertSame($expectedNormd, $normd);
+        $this->assertSame($expectedSteinitz, $steinitz);
+        $this->assertSame($expectedMean, $mean);
+        $this->assertSame($expectedMedian, $median);
+        $this->assertSame($expectedMode, $mode);
+        $this->assertSame($expectedVar, $var);
+        $this->assertSame($expectedSd, $sd);
     }
 }
