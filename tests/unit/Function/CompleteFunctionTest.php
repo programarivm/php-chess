68380b0a371f7a8946ea74fdadb89a5a903c1d0e
standard <info@programarivm.com>
2025-01-15 12:26:56 +0100

    Updated CompleteFunctionTest

--- tests/unit/Function/CompleteFunctionTest.php
+++ tests/unit/Function/CompleteFunctionTest.php
@@ -5,7 +5,6 @@ namespace Chess\Tests\Unit\Function;
 use Chess\FenToBoardFactory;
 use Chess\Function\CompleteFunction;
 use Chess\Tests\AbstractUnitTestCase;
-use Chess\Variant\Classical\FEN\StrToBoard;
 
 class CompleteFunctionTest extends AbstractUnitTestCase
 {
@@ -69,21 +68,21 @@ class CompleteFunctionTest extends AbstractUnitTestCase
      */
     public function absolute_fork()
     {
-        $board = FenToBoardFactory::create('8/8/5k1n/6P1/7K/8/8/8 w - -');
-
         $expectedResult = [
             'w' => 3.2,
             'b' => 0,
         ];
 
-        $expectedPhrase = [
+        $expectedElaboration = [
             "Absolute fork attack on the knight on h6.",
         ];
+
+        $board = FenToBoardFactory::create('8/8/5k1n/6P1/7K/8/8/8 w - -');
 
         $eval = CompleteFunction::evaluate('Absolute fork', $board);
 
         $this->assertSame($expectedResult, $eval->result);
-        $this->assertSame($expectedPhrase, $eval->elaborate());
+        $this->assertSame($expectedElaboration, $eval->elaborate());
     }
 
     /**
@@ -91,21 +90,21 @@ class CompleteFunctionTest extends AbstractUnitTestCase
      */
     public function absolute_pin()
     {
-        $board = FenToBoardFactory::create('r1bqkbnr/ppp2ppp/2np4/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R w KQkq -');
-
         $expectedResult = [
             'w' => 0,
             'b' => 3.2,
         ];
 
-        $expectedPhrase = [
+        $expectedElaboration = [
             "The knight on c6 is pinned shielding the king so it cannot move out of the line of attack because the king would be put in check.",
         ];
+
+        $board = FenToBoardFactory::create('r1bqkbnr/ppp2ppp/2np4/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R w KQkq -');
 
         $eval = CompleteFunction::evaluate('Absolute pin', $board);
 
         $this->assertSame($expectedResult, $eval->result);
-        $this->assertSame($expectedPhrase, $eval->elaborate());
+        $this->assertSame($expectedElaboration, $eval->elaborate());
     }
 
     /**
@@ -118,7 +117,7 @@ class CompleteFunctionTest extends AbstractUnitTestCase
             'b' => 1,
         ];
 
-        $expectedPhrase = [
+        $expectedElaboration = [
             "When White's king on e4 will be moved, a piece that is more valuable than the bishop on d5 may well be exposed to attack.",
         ];
 
@@ -128,6 +127,6 @@ class CompleteFunctionTest extends AbstractUnitTestCase
         $eval = CompleteFunction::evaluate('Absolute skewer', $board);
 
         $this->assertSame($expectedResult, $eval->result);
-        $this->assertSame($expectedPhrase, $eval->elaborate());
+        $this->assertSame($expectedElaboration, $eval->elaborate());
     }
 }
