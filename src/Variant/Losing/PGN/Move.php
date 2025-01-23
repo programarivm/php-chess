aa74d7f8d4dcf16f6c98bcc81f76d9a7d3a3075e
standard <info@programarivm.com>
2025-01-23 11:50:33 +0100

    Updated Chess\Variant\Losing\PGN\Move

--- src/Variant/Losing/PGN/Move.php
+++ src/Variant/Losing/PGN/Move.php
@@ -10,6 +10,6 @@ class Move extends ClassicalPgnMove
 {
     const PAWN_PROMOTES = '[a-h]{1}(1|8){1}' . '[=]{0,1}[NBRQM]{0,1}' . Check::REGEX;
     const PAWN_CAPTURES_AND_PROMOTES = '[a-h]{1}x' . '[a-h]{1}(1|8){1}' . '[=]{0,1}[NBRQM]{0,1}' . Check::REGEX;
-    const PIECE = '[BRQM]{1}[a-h]{0,1}[1-8]{0,1}' . Square::REGEX . Check::REGEX;
-    const PIECE_CAPTURES = '[BRQM]{1}[a-h]{0,1}[1-8]{0,1}x' . Square::REGEX . Check::REGEX;
+    const PIECE = '[BMNQR]{1}[a-h]{0,1}[1-8]{0,1}' . Square::REGEX . Check::REGEX;
+    const PIECE_CAPTURES = '[BMNQR]{1}[a-h]{0,1}[1-8]{0,1}x' . Square::REGEX . Check::REGEX;
 }
