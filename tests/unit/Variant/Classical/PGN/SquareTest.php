3924da9b81499be2a874ce62d496b3eec1a047ee
standard <info@programarivm.com>
2025-01-23 15:43:55 +0100

    Updated tests

--- tests/unit/Variant/Classical/PGN/SquareTest.php
+++ tests/unit/Variant/Classical/PGN/SquareTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Chess\Tests\Unit\Variant\Classical\PGN\AN;
+namespace Chess\Tests\Unit\Variant\Classical\PGN;
 
 use Chess\Exception\UnknownNotationException;
 use Chess\Variant\Classical\PGN\Square;
