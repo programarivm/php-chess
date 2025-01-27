3a73966b1b5fa719cfa59a80ac0690600118b4dd
standard <info@programarivm.com>
2025-01-27 10:09:54 +0100

    Improved tests

--- tests/unit/Variant/Classical/BTest.php
+++ tests/unit/Variant/Classical/BTest.php
@@ -23,15 +23,16 @@ class BTest extends AbstractUnitTestCase
      */
     public function flow_a2()
     {
-        $bishop = new B('w', 'a2', self::$square);
-        $flow = [
+        $expected = [
             0 => [],
             1 => ['b3', 'c4', 'd5', 'e6', 'f7', 'g8'],
             2 => [],
             3 => ['b1'],
         ];
 
-        $this->assertEquals($flow, $bishop->flow);
+        $flow = (new B('w', 'a2', self::$square))->flow;
+
+        $this->assertEquals($expected, $flow);
     }
 
     /**
@@ -39,15 +40,16 @@ class BTest extends AbstractUnitTestCase
      */
     public function flow_d5()
     {
-        $bishop = new B('w', 'd5', self::$square);
-        $flow = [
+        $expected = [
             0 => ['c6', 'b7', 'a8'],
             1 => ['e6', 'f7', 'g8'],
             2 => ['c4', 'b3', 'a2'],
             3 => ['e4', 'f3', 'g2', 'h1'],
         ];
 
-        $this->assertEquals($flow, $bishop->flow);
+        $flow = (new B('w', 'd5', self::$square))->flow;
+
+        $this->assertEquals($expected, $flow);
     }
 
     /**
@@ -55,15 +57,16 @@ class BTest extends AbstractUnitTestCase
      */
     public function flow_a8()
     {
-        $bishop = new B('w', 'a8', self::$square);
-        $flow = [
+        $expected = [
             0 => [],
             1 => [],
             2 => [],
             3 => ['b7', 'c6', 'd5', 'e4', 'f3', 'g2', 'h1'],
         ];
 
-        $this->assertEquals($flow, $bishop->flow);
+        $flow = (new B('w', 'a8', self::$square))->flow;
+
+        $this->assertEquals($expected, $flow);
     }
 
     /**
@@ -71,14 +74,15 @@ class BTest extends AbstractUnitTestCase
      */
     public function line_of_attack_e4()
     {
-        $board = FenToBoardFactory::create('8/1k6/8/8/4B3/4K3/8/8 b - -');
-
         $expected = [
             'c6',
             'd5',
         ];
 
-        $this->assertEquals($expected, $board->pieceBySq('e4')->line($board->piece(Color::B, Piece::K)->sq));
+        $board = FenToBoardFactory::create('8/1k6/8/8/4B3/4K3/8/8 b - -');
+        $line = $board->pieceBySq('e4')->line($board->piece(Color::B, Piece::K)->sq);
+
+        $this->assertEquals($expected, $line);
     }
 
     /**
@@ -86,14 +90,15 @@ class BTest extends AbstractUnitTestCase
      */
     public function line_of_attack_e5()
     {
-        $board = FenToBoardFactory::create('7k/8/8/4B3/8/4K3/8/8 b - -');
-
         $expected = [
             'g7',
             'f6',
         ];
 
-        $this->assertEquals($expected, $board->pieceBySq('e5')->line($board->piece(Color::B, Piece::K)->sq));
+        $board = FenToBoardFactory::create('7k/8/8/4B3/8/4K3/8/8 b - -');
+        $line = $board->pieceBySq('e5')->line($board->piece(Color::B, Piece::K)->sq);
+
+        $this->assertEquals($expected, $line);
     }
 
     /**
@@ -101,15 +106,16 @@ class BTest extends AbstractUnitTestCase
      */
     public function line_of_attack_d6()
     {
-        $board = FenToBoardFactory::create('8/8/3B4/8/8/4K3/7k/8 b - -');
-
         $expected = [
             'g3',
             'f4',
             'e5',
         ];
 
-        $this->assertEquals($expected, $board->pieceBySq('d6')->line($board->piece(Color::B, Piece::K)->sq));
+        $board = FenToBoardFactory::create('8/8/3B4/8/8/4K3/7k/8 b - -');
+        $line = $board->pieceBySq('d6')->line($board->piece(Color::B, Piece::K)->sq);
+
+        $this->assertEquals($expected, $line);
     }
 
     /**
@@ -117,8 +123,6 @@ class BTest extends AbstractUnitTestCase
      */
     public function line_of_attack_g6()
     {
-        $board = FenToBoardFactory::create('8/8/6B1/8/8/4K3/8/1k6 b - -');
-
         $expected = [
             'c2',
             'd3',
@@ -126,7 +130,10 @@ class BTest extends AbstractUnitTestCase
             'f5',
         ];
 
-        $this->assertEquals($expected, $board->pieceBySq('g6')->line($board->piece(Color::B, Piece::K)->sq));
+        $board = FenToBoardFactory::create('8/8/6B1/8/8/4K3/8/1k6 b - -');
+        $line = $board->pieceBySq('g6')->line($board->piece(Color::B, Piece::K)->sq);
+
+        $this->assertEquals($expected, $line);
     }
 
     /**
