65549905b55f498db7d9f9bbe539fe158ef91152
standard <info@programarivm.com>
2025-01-27 10:16:06 +0100

    Improved tests

--- tests/unit/Variant/Classical/RTest.php
+++ tests/unit/Variant/Classical/RTest.php
@@ -24,15 +24,16 @@ class RTest extends AbstractUnitTestCase
      */
     public function flow_a2()
     {
-        $rook = new R('w', 'a2', self::$square, RType::R);
-        $flow = [
+        $expected = [
             0 => ['a3', 'a4', 'a5', 'a6', 'a7', 'a8'],
             1 => ['a1'],
             2 => [],
             3 => ['b2', 'c2', 'd2', 'e2', 'f2', 'g2', 'h2'],
         ];
 
-        $this->assertEquals($flow, $rook->flow);
+        $flow = (new R('w', 'a2', self::$square, RType::R))->flow;
+
+        $this->assertEquals($expected, $flow);
     }
 
     /**
@@ -40,15 +41,16 @@ class RTest extends AbstractUnitTestCase
      */
     public function flow_d5()
     {
-        $rook = new R('w', 'd5', self::$square, RType::R);
-        $flow = [
+        $expected = [
             0 => ['d6', 'd7', 'd8'],
             1 => ['d4', 'd3', 'd2', 'd1'],
             2 => ['c5', 'b5', 'a5'],
             3 => ['e5', 'f5', 'g5', 'h5'],
         ];
 
-        $this->assertEquals($flow, $rook->flow);
+        $flow = (new R('w', 'd5', self::$square, RType::R))->flow;
+
+        $this->assertEquals($expected, $flow);
     }
 
     /**
@@ -56,8 +58,6 @@ class RTest extends AbstractUnitTestCase
      */
     public function line_of_attack_b7()
     {
-        $board = FenToBoardFactory::create('4K3/1R6/8/8/8/8/1k6/8 b - -');
-
         $expected = [
             'b3',
             'b4',
@@ -65,7 +65,10 @@ class RTest extends AbstractUnitTestCase
             'b6',
         ];
 
-        $this->assertEquals($expected, $board->pieceBySq('b7')->line($board->piece(Color::B, Piece::K)->sq));
+        $board = FenToBoardFactory::create('4K3/1R6/8/8/8/8/1k6/8 b - -');
+        $line = $board->pieceBySq('b7')->line($board->piece(Color::B, Piece::K)->sq);
+
+        $this->assertEquals($expected, $line);
     }
 
     /**
@@ -73,8 +76,6 @@ class RTest extends AbstractUnitTestCase
      */
     public function line_of_attack_c2()
     {
-        $board = FenToBoardFactory::create('4K3/2k5/8/8/8/8/2R5/8 b - -');
-
         $expected = [
             'c6',
             'c5',
@@ -82,7 +83,10 @@ class RTest extends AbstractUnitTestCase
             'c3',
         ];
 
-        $this->assertEquals($expected, $board->pieceBySq('c2')->line($board->piece(Color::B, Piece::K)->sq));
+        $board = FenToBoardFactory::create('4K3/2k5/8/8/8/8/2R5/8 b - -');
+        $line = $board->pieceBySq('c2')->line($board->piece(Color::B, Piece::K)->sq);
+
+        $this->assertEquals($expected, $line);
     }
 
     /**
@@ -90,8 +94,6 @@ class RTest extends AbstractUnitTestCase
      */
     public function line_of_attack_a4()
     {
-        $board = FenToBoardFactory::create('7k/8/8/8/r4K2/8/8/8 w - -');
-
         $expected = [
             'e4',
             'd4',
@@ -99,9 +101,10 @@ class RTest extends AbstractUnitTestCase
             'b4',
         ];
 
-        $sq = $board->piece(Color::W, Piece::K)->sq;
+        $board = FenToBoardFactory::create('7k/8/8/8/r4K2/8/8/8 w - -');
+        $line = $board->pieceBySq('a4')->line($board->piece(Color::W, Piece::K)->sq);
 
-        $this->assertEquals($expected, $board->pieceBySq('a4')->line($sq));
+        $this->assertEquals($expected, $line);
     }
 
     /**
@@ -109,8 +112,6 @@ class RTest extends AbstractUnitTestCase
      */
     public function line_of_attack_f5()
     {
-        $board = FenToBoardFactory::create('7k/8/8/K4r2/8/8/8/8 w - -');
-
         $expected = [
             'b5',
             'c5',
@@ -118,6 +119,9 @@ class RTest extends AbstractUnitTestCase
             'e5',
         ];
 
-        $this->assertEquals($expected, $board->pieceBySq('f5')->line($board->piece(Color::W, Piece::K)->sq));
+        $board = FenToBoardFactory::create('7k/8/8/K4r2/8/8/8/8 w - -');
+        $line = $board->pieceBySq('f5')->line($board->piece(Color::W, Piece::K)->sq);
+
+        $this->assertEquals($expected, $line);
     }
 }
