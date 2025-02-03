ff81939f2fcb0f5582d5cce164f0a97d9b28f8f3
standard <info@programarivm.com>
2025-02-03 11:35:26 +0100

    Updated Chess\Variant\PieceArray

--- src/Variant/PieceArray.php
+++ src/Variant/PieceArray.php
@@ -11,21 +11,14 @@ use Chess\Variant\Classical\CastlingRule;
 
 class PieceArray
 {
-    public Square $square;
-
-    public ?CastlingRule $castlingRule;
-
     public array $pieces;
 
     public function __construct(array $array, Square $square, CastlingRule $castlingRule = null, string $namespace)
     {
-        $this->square = $square;
-        $this->castlingRule = $castlingRule;
-
         foreach ($array as $i => $rank) {
             foreach ($rank as $j => $char) {
                 if ($char !== '.') {
-                    $sq = $this->square->toAn($j, $i);
+                    $sq = $square->toAn($j, $i);
                     if (ctype_lower($char)) {
                         $color = Color::B;
                         $char = strtoupper($char);
@@ -33,16 +26,16 @@ class PieceArray
                         $color = Color::W;
                     }
                     if ($char === Piece::R) {
-                        if ($sq === $this->castlingRule?->rule[$color][Piece::R][Castle::LONG]['from']) {
-                            $this->pieces[] = new R($color, $sq, $this->square, RType::CASTLE_LONG);
-                        } elseif ($sq === $this->castlingRule?->rule[$color][Piece::R][Castle::SHORT]['from']) {
-                            $this->pieces[] = new R($color, $sq, $this->square, RType::CASTLE_SHORT);
+                        if ($sq === $castlingRule?->rule[$color][Piece::R][Castle::LONG]['from']) {
+                            $this->pieces[] = new R($color, $sq, $square, RType::CASTLE_LONG);
+                        } elseif ($sq === $castlingRule?->rule[$color][Piece::R][Castle::SHORT]['from']) {
+                            $this->pieces[] = new R($color, $sq, $square, RType::CASTLE_SHORT);
                         } else {
-                            $this->pieces[] = new R($color, $sq, $this->square, RType::R);
+                            $this->pieces[] = new R($color, $sq, $square, RType::R);
                         }
                     } else {
                         $class = VariantType::getClass($char, $namespace);
-                        $this->pieces[] = new $class($color, $sq, $this->square);
+                        $this->pieces[] = new $class($color, $sq, $square);
                     }
                 }
             }
