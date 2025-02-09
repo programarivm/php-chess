364d84bd4a7ebae32daed06d26707ddb8dcab237
standard <info@programarivm.com>
2025-02-09 12:25:40 +0100

    Futurized the chess tutor paragraphs

--- src/Tutor/AbstractParagraph.php
+++ src/Tutor/AbstractParagraph.php
@@ -12,4 +12,16 @@ abstract class AbstractParagraph
     public AbstractBoard $board;
 
     public array $paragraph = [];
+
+    public function futurize() 
+    {
+        foreach ($this->paragraph as &$val) {
+            $val = preg_replace('/\bare\b/u', 'will be', $val);
+            $val = preg_replace('/\bcan\b/u', 'will be able to', $val);
+            $val = preg_replace('/\bhas\b/u', 'will have', $val);
+            $val = preg_replace('/\bis\b/u', 'will be', $val);
+        }
+
+        return $this->paragraph;
+    }
 }
