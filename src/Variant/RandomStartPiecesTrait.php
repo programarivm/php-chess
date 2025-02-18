a9056652024c3fcb991ada8f7c357f854bc56003
standard <info@programarivm.com>
2025-02-18 22:58:12 +0100

    Refactored RandomStartPiecesTrait

--- src/Variant/RandomStartPiecesTrait.php
+++ src/Variant/RandomStartPiecesTrait.php
@@ -11,10 +11,9 @@ trait RandomStartPiecesTrait
 {
     protected array $shuffle;
 
-    protected array $pieces = [];
-
     public function pieces(): array
     {
+        $pieces = [];
         $longCastlingRook = null;
 
         foreach ($this->shuffle as $key => $val) {
@@ -22,25 +21,25 @@ trait RandomStartPiecesTrait
             $bSq = chr(97 + $key) . $this->square::SIZE['ranks'];
             $class = VariantType::getClass($val, $this->namespace);
             if ($val !== Piece::R) {
-                $this->pieces[] =  new $class(Color::W, $wSq, $this->square);
-                $this->pieces[] =  new $class(Color::B, $bSq, $this->square);
+                $pieces[] =  new $class(Color::W, $wSq, $this->square);
+                $pieces[] =  new $class(Color::B, $bSq, $this->square);
             } elseif (!$longCastlingRook) {
-                $this->pieces[] =  new $class(Color::W, $wSq, $this->square, RType::CASTLE_LONG);
-                $this->pieces[] =  new $class(Color::B, $bSq, $this->square, RType::CASTLE_LONG);
+                $pieces[] =  new $class(Color::W, $wSq, $this->square, RType::CASTLE_LONG);
+                $pieces[] =  new $class(Color::B, $bSq, $this->square, RType::CASTLE_LONG);
                 $longCastlingRook = $this->shuffle[$key];
             } else {
-                $this->pieces[] =  new $class(Color::W, $wSq, $this->square, RType::CASTLE_SHORT);
-                $this->pieces[] =  new $class(Color::B, $bSq, $this->square, RType::CASTLE_SHORT);
+                $pieces[] =  new $class(Color::W, $wSq, $this->square, RType::CASTLE_SHORT);
+                $pieces[] =  new $class(Color::B, $bSq, $this->square, RType::CASTLE_SHORT);
             }
         }
 
         for ($i = 0; $i < $this->square::SIZE['files']; $i++) {
             $wSq = chr(97 + $i) . 2;
             $bSq = chr(97 + $i) . $this->square::SIZE['ranks'] - 1;
-            $this->pieces[] = new P(Color::W, $wSq, $this->square);
-            $this->pieces[] = new P(Color::B, $bSq, $this->square);
+            $pieces[] = new P(Color::W, $wSq, $this->square);
+            $pieces[] = new P(Color::B, $bSq, $this->square);
         }
 
-        return $this->pieces;
+        return $pieces;
     }
 }
